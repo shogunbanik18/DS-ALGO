53a00f9f822a55c3aca981d42221c7a5e4546748
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-10-20 13:32:29 +0530

    Create Kadanes_Algo.cpp

---
+++ Luv Babbar dsa/Array/Kadanes_Algo.cpp
@@ -0,0 +1,27 @@
+// Kadanes Algo
+// Maximum sum subarray 
+
+class solution{   
+public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        int maxsum=INT_MIN;
+        int cursum=0;
+        for(int i=0;i<n;i++)
+        {
+            cursum=cursum+arr[i];
+            if(maxsum<cursum)
+            {
+                maxsum=cursum;
+            }
+            
+            if(cursum<0)
+            {
+                cursum=0;
+            }
+        }
+        return maxsum;
+   }
+};
