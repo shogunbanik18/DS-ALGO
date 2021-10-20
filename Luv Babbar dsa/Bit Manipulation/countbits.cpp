203c839983eb1368686a7227be2bfcbff52954d8
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-10-20 18:23:34 +0530

    Update countbits.cpp

--- Luv Babbar dsa/Bit Manipulation/countbits.cpp
+++ Luv Babbar dsa/Bit Manipulation/countbits.cpp
@@ -14,10 +14,22 @@ int count_bits(int n)
     return count;
 }
 
+int count_bits_hack(int n)
+{
+    int ans = 0;
+    while (n > 0)
+    {
+        n = n & (n - 1);
+        ans++;
+    }
+    return ans;
+}
+
 int main()
 {
     int n;
     cin >> n;
     cout << count_bits(n) << endl;
+    cout << count_bits_hack(n) << endl;
     return 0;
 }
