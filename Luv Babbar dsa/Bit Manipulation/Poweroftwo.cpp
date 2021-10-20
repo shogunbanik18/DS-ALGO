cc5878ff25faf95c4575e8e5c1f6548df5c3fd18
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-10-20 21:17:26 +0530

    Update Poweroftwo.cpp

--- Luv Babbar dsa/Bit Manipulation/Poweroftwo.cpp
+++ Luv Babbar dsa/Bit Manipulation/Poweroftwo.cpp
@@ -19,3 +19,19 @@ int main()
 
     return 0;
 }
+
+
+// class Solution{
+//     public:
+//     // Function to check if given number n is a power of two.
+//     bool isPowerofTwo(long long n)
+//     {
+//         if(n==0)
+//         {
+//             return false;
+//         }
+//        return (!(n&(n-1)));
+//         // Your code here    
+        
+//     }
+// };
