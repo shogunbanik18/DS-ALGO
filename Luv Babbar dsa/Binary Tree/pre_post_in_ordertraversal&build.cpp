be78e7e25468e80b4fc46a8c16eed344d50f119d
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-10-25 20:41:37 +0530

    pre_post_in_ordertraversal&build.cpp

--- Luv Babbar dsa/Binary Tree/pre_post_in_ordertraversal&build.cpp
+++ Luv Babbar dsa/Binary Tree/pre_post_in_ordertraversal&build.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 // input : 1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
 
+// output
+// preorder : 1 2 4 5 7 3 6
+// inorder : 4,2,7,5,1,3,6
+// postorder : 4,7,5,2,6,3,1
+
 class Node
 {
 public:
@@ -36,6 +41,7 @@ Node *buildtree()
 }
 
 // printing the tree
+// preorder traversal
 void printpreorder(Node *root)
 {
     // base case
@@ -49,8 +55,38 @@ void printpreorder(Node *root)
     printpreorder(root->right);
 }
 
+// inorder traversal
+void printinorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    printinorder(root->left);
+    cout << root->data << " ";
+    printinorder(root->right);
+}
+
+// Postorder traversal
+void printpostorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    printpostorder(root->left);
+    printpostorder(root->right);
+    cout << root->data << " ";
+}
+
 int main()
 {
     Node *root = buildtree();
-    printpreorder(root);
+    // printpreorder(root);
+    // printinorder(root);
+    printpostorder(root);
 }
