ab27dc72824f62c61d7bb00e1a687e7329763613
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-10-29 00:34:43 +0530

    Create Palindromic_array.cpp

---
+++ Luv Babbar dsa/Array/Palindromic_array.cpp
@@ -0,0 +1,19 @@
+int PalinArray(int a[], int n)
+{ 
+   for(int i=0;i<n;i++)
+   {
+       int ans=0;
+       int temp=a[i];
+       while(temp>0)
+       {
+           int r=temp%10;
+           temp=temp/10;
+           ans=(ans*10)+r;
+       }
+       if(ans!=a[i])
+       {
+           return 0;
+       }
+   }
+     return 1;
+}
