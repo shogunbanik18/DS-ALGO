df946d7e36d26bb849f552145749d7a7713ce251
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-11-09 16:31:23 +0530

    Create queue.h

---
+++ Luv Babbar dsa/Stack&queues/Stacks_and_queues/queue.h
@@ -0,0 +1,52 @@
+class Queue
+{
+    int *arr;
+    int cs;
+    int ms;
+    int front;
+    int rear;
+
+public:
+    Queue(int default_size = 5)
+    {
+        ms = default_size;
+        arr = new int[ms];
+        cs = 0;
+        front = 0;
+        rear = ms - 1;
+    }
+
+    bool full()
+    {
+        return cs == ms;
+    }
+
+    bool empty()
+    {
+        return cs == 0;
+    }
+
+    void push(int data)
+    {
+        if (!full())
+        {
+            rear = (rear + 1) % ms;
+            arr[rear] = data;
+            cs++;
+        }
+    }
+
+    void pop()
+    {
+        if (!empty())
+        {
+            front = (front + 1) % ms;
+            cs--;
+        }
+    }
+
+    int getfront()
+    {
+        return arr[front];
+    }
+};
