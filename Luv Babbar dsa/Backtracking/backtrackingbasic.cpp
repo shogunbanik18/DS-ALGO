70d5f688e6451ee496c288641f04c7688c0b0548
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-11-19 19:19:29 +0530

    Update backtrackingbasic.cpp

--- Luv Babbar dsa/Backtracking/backtrackingbasic.cpp
+++ Luv Babbar dsa/Backtracking/backtrackingbasic.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printarray(int *arr, int n)
+void printarray(vector<int> &arr, int n)
 {
     for (int i = 0; i < n; i++)
     {
@@ -9,7 +9,7 @@ void printarray(int *arr, int n)
     }
 }
 
-void fillarray(int *arr, int i, int n, int val)
+void fillarray(vector<int> &arr, int i, int n, int val)
 {
     // base case
     if (i == n)
@@ -19,16 +19,21 @@ void fillarray(int *arr, int i, int n, int val)
     }
     arr[i] = val;
     fillarray(arr, i + 1, n, val + 1);
-    // Backtracking step 
-    arr[i]=-1*arr[i];
+    // Backtracking step
+    arr[i] = -1 * arr[i];
 }
 
 int main()
 {
-    int arr[100] = {0};
+    // int arr[100] = {0};
+    vector<int> arr(100, 0);
     int n;
     cin >> n;
     fillarray(arr, 0, n, 1);
     cout << endl;
     printarray(arr, n);
 }
+
+// vectors (STL data structures are passed by value)
+// to pass it by reference we use & sign
+// vector<int> &arr
