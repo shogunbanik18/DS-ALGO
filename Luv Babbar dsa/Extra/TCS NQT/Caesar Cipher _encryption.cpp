d4945445e45d3628294bcc6bf1f85462678d0941
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-09 20:39:36 +0530

    Update and rename Caesar Cipher _encryption to Caesar Cipher _encryption.cpp

--- Luv Babbar dsa/Extra/TCS NQT/Caesar Cipher _encryption.cpp
+++ Luv Babbar dsa/Extra/TCS NQT/Caesar Cipher _encryption.cpp
@@ -32,6 +32,27 @@ void caesarcipher(string s,int n,int k)
     }
 }
 
+
+// Alter //
+
+string caesarcipher(string s,int k)
+{
+    string res="";
+    for(int i=0;i<s.length();i++)
+    {
+        if(isupper(s[i]))
+        {
+            res=res +char(int (s[i]+k-65)%26+65) ; 
+        }
+        else
+        {
+            res=res +char(int (s[i]+k-97)%26+97);
+        }
+    }
+    return res;
+}
+
+
 int main()
 {
     // int n;
