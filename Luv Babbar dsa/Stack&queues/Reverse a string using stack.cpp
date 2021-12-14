7a2e38a90150a3804879b8a5c95c705befb41c18
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-14 13:04:47 +0530

    Create Reverse a string using stack.cpp

---
+++ Luv Babbar dsa/Stack&queues/Reverse a string using stack.cpp
@@ -0,0 +1,19 @@
+//return the address of the string
+// tc: O(N)
+// sc: O(1)
+char* reverse(char *S, int len)
+{
+    stack<char>st;
+    for(int i=0;i<len;i++)
+    {
+        st.push(S[i]);
+    }
+    
+    for(int i=0;i<len;i++)
+    {
+        S[i]=st.top();
+        st.pop();
+    }
+    return S;
+    //code here
+}
