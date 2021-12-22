8e76078d2ab4a9df479518ebdc4ff2a3d466cabc
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-22 20:14:53 +0530

    Update and rename Palindrome Linked list to Palindrome Linked list.cpp

--- Luv Babbar dsa/LinkedList/Palindrome Linked list.cpp
+++ Luv Babbar dsa/LinkedList/Palindrome Linked list.cpp
@@ -14,37 +14,37 @@ class Solution{
     //Function to check whether the list is palindrome.
     // // using extra space
     // // O(N) space and time 
-    // Node* reverse(Node*head)
-    // {
-    //     Node*curr1=head;
-    //     Node*prev=NULL;
-    //     Node*temp;
-    //     while(curr1!=NULL)
-    //     {
-    //         temp=curr1->next;
-    //         curr1->next=prev;
-    //         prev=curr1;
-    //         curr1=temp;
-    //     }
-    //     return prev;
-    // }
+    Node* reverse(Node*head)
+    {
+        Node*curr1=head;
+        Node*prev=NULL;
+        Node*temp;
+        while(curr1!=NULL)
+        {
+            temp=curr1->next;
+            curr1->next=prev;
+            prev=curr1;
+            curr1=temp;
+        }
+        return prev;
+    }
     
-    // bool isPalindrome(Node *head)
-    // {
-    //     Node*temp=head;
-    //     Node*curr=reverse(head);
-    //     while(temp!=NULL and curr!=NULL)
-    //     {
-    //         if(temp->data==curr->data)
-    //         {
-    //             return true;
-    //             temp=temp->next;
-    //             curr=curr->next;
-    //         }
-    //     }
-    //     return false;
-    //     //Your code here
-    // }
+    bool isPalindrome(Node *head)
+    {
+        Node*temp=head;
+        Node*curr=reverse(head);
+        while(temp!=NULL and curr!=NULL)
+        {
+            if(temp->data==curr->data)
+            {
+                return true;
+                temp=temp->next;
+                curr=curr->next;
+            }
+        }
+        return false;
+        //Your code here
+    }
     
     
     // using tc: O(N) 
@@ -99,3 +99,41 @@ class Solution{
         return true;
     }
 };
+
+
+
+
+// leetcode 
+ // using vector 
+    // tc: O(N)
+    // sc: O(N)
+    bool isPalindrome(ListNode* head) 
+    {
+        vector<int>v;
+        ListNode*curr=head;
+        while(curr!=NULL)
+        {
+            v.push_back(curr->val);
+            curr=curr->next;
+        }
+        
+        bool flag=true;
+        int i=0;
+        int j=v.size()-1;
+        while(i<=j)
+        {
+            if(v[i]!=v[j])
+            {
+                flag=false;
+            }
+            i++;
+            j--;
+        }
+        
+        if(flag==false)
+        {
+            return false;
+        }
+        return true;  
+    }
+    
