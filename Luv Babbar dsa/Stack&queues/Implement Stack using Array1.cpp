6d40dfcbf164271a642a90972481af573e380cd4
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-22 19:07:14 +0530

    Update Implement Stack using Array1.cpp

--- Luv Babbar dsa/Stack&queues/Implement Stack using Array1.cpp
+++ Luv Babbar dsa/Stack&queues/Implement Stack using Array1.cpp
@@ -15,3 +15,59 @@ int MyStack :: pop()
     return arr[top--];
     // Your Code       
 }
+
+
+// alter 
+
+#include<iostream>
+using namespace std;
+
+struct stack
+{
+    int arr[5];
+    int top=-1;
+    
+    void push(int element)
+    {
+        if(top==4)
+        {
+            cout<<"Error"<<endl;
+        }
+        else
+        {
+            top++;
+            arr[top]=element;
+        }
+    }
+    
+    void pop()
+    {
+        if(top==-1)
+        {
+            cout<<"Error"<<endl;
+        }
+        else
+        {
+            top--;
+        }
+    }
+    
+    void print()
+    {
+        for(int m=0;m<=top;m++)
+        {
+            cout<<arr[m]<<endl;
+        }
+    }
+};
+
+int main(){
+    stack s;
+    s.push(2);
+    s.push(3);
+    s.pop();
+    s.push(4) ;
+    s.pop();
+    s.print();
+}
+
