822b1e4d8599a549c7e142b189a7e2fb277f2e63
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-26 23:58:24 +0530

    Update Adjacency List.cpp

--- Luv Babbar dsa/Graph/Striver Graph series/Adjacency List.cpp
+++ Luv Babbar dsa/Graph/Striver Graph series/Adjacency List.cpp
@@ -1,51 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Adjacency matrix 
-// sc: o(n*n)
-// int main()
-// {
-//     // n=nodes 
-//     // m=edges 
-//     int n,m;
-//     cin>>n>>m;
-    
-//     int adj[n+1][n+1];
-    
-//     for(int i=0;i<m;i++)
-//     {
-//         int u,v;
-//         cin>>u>>v;
-//         adj[u][v]=1;
-//         adj[v][u]=1;
-//     }
-//     return 0;
-// }
+// tc: O(N)+2E
+// sc:O(N)+2E
 
-
-// Adjacency List representation 
-// sc : (N+2E)
 int main()
 {
-    // n=nodes 
+    // n=nodes
     // m=edges 
     int n,m;
     cin>>n>>m;
     
-    // incase of weighted graph 
-    // use vector<pair<int,int>>adj[n+1]
-    // adj[u].push_back({v,w}) u,v,w
-    
+    // undirected graph
     vector<int>adj[n+1];
     for(int i=0;i<m;i++)
     {
         int u,v;
         cin>>u>>v;
         
-        // for undirected graph 
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
     
+    
+    // directed graph 
+    // vector<int>adj[n+1];
+    // for(int i=0;i<m;i++)
+    // {
+    //     int u,v;
+    //     cin>>u>>v;
+        
+    //     adj[u].push_back(v);
+    // }
+    
+    
+    // weighted graph 
+    // vector<pair<int,int>>adj[n+1];
+    // for(int i=0;i<m;i++)
+    // {
+    //     int u,v,w;
+    //     cin>>u>>v>>W;
+        
+    //     adj[u].push_back({v,w});
+    //     adj[v].push_back({u,w});
+    // }
     return 0;
 }
