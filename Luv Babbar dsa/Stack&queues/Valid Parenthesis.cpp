c8cf236cd18e3f0730c06892cf0fe9911cf1fbd3
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-26 17:11:49 +0530

    Create Valid Parenthesis.cpp

---
+++ Luv Babbar dsa/Stack&queues/Valid Parenthesis.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        bool ans=true;
+        stack<char>st;
+        int n=s.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' or s[i]=='{' or s[i]=='[')
+            {
+                st.push(s[i]);
+            }
+            
+            else if(s[i]=='}')
+            {
+                if(!st.empty() and st.top()=='{')
+                {
+                    st.pop();
+                }
+                 else
+                 {
+                        ans = false;
+                        break;
+                }
+            }
+            
+            else if(s[i]==']')
+            {
+                if(!st.empty() and st.top()=='[')
+                {
+                    st.pop();
+                }
+                 else
+                 {
+                        ans = false;
+                        break;
+                }
+            }
+            
+            else if(s[i]==')')
+            {
+                if(!st.empty() and st.top()=='(')
+                {
+                    st.pop();
+                }
+                 else
+                 {
+                        ans = false;
+                        break;
+                }
+                
+            }
+        }
+        if(!st.empty())
+        {
+            return false;
+        }
+        return ans;
+    }
+};
