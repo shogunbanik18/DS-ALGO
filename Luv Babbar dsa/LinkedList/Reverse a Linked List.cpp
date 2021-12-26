34f4ef58220f64d23b49e4929177298ad78eb46c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-26 14:04:09 +0530

    Update and rename Reverse a Linked List to Reverse a Linked List.cpp

--- Luv Babbar dsa/LinkedList/Reverse a Linked List.cpp
+++ Luv Babbar dsa/LinkedList/Reverse a Linked List.cpp
@@ -106,3 +106,33 @@ int main()
 //     reverse(head);
 //     cout << head;
 // }
+
+
+Leet code 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) 
+    {
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*temp=NULL;
+        while(curr!=NULL)
+        {
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+};
