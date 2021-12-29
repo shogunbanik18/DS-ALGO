4b0bf6f2aa4f63590224f108684cc795f551665b
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2021-12-29 08:50:35 +0530

    Update Rotate a Linked list By K.cpp

--- Luv Babbar dsa/LinkedList/Rotate a Linked list By K.cpp
+++ Luv Babbar dsa/LinkedList/Rotate a Linked list By K.cpp
@@ -73,3 +73,45 @@ class Solution
 };
     
     
+Leetcode 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k)
+    {
+        // base case 
+       if(!head or !head->next or k==0)
+       {
+           return head;
+       }
+        ListNode*curr=head;
+        int count=1;
+        while(curr->next)
+        {
+            count++;
+            curr=curr->next;
+        }
+        curr->next=head;
+        if(k>count)
+        {
+            k=k%count;
+        }
+        int x=count-k;
+        while(x--)
+        {
+            curr=curr->next;
+        }
+        head=curr->next;
+        curr->next=NULL;
+        return head;
+    }
+};
