3ed1307d885b05ee13521aed93af49280757887f
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-05 02:14:06 +0530

    Update Check if All A's Appears Before All B's.cpp

--- Luv Babbar dsa/Leetcode/Check if All A's Appears Before All B's.cpp
+++ Luv Babbar dsa/Leetcode/Check if All A's Appears Before All B's.cpp
@@ -2,12 +2,27 @@ class Solution {
 public:
     bool checkString(string s) 
     {
-        for(int i=1;i<s.size();i++)
+        // for(int i=1;i<s.size();i++)
+        // {
+        //         if(s[i-1]=='b' and s[i]=='a')
+        //         {
+        //             return false;
+        //         }
+        // }
+        // return true;
+        if(s[0]=='b' and s[1]=='a')
         {
-                if(s[i-1]=='b' and s[i]=='a')
+            return false;
+        }
+        for(int i=1;i<s.size()-1;i++)
+        {
+            if(s[i]=='b')
+            {
+                if( s[i+1]=='a')
                 {
                     return false;
                 }
+            }
         }
         return true;
     }
