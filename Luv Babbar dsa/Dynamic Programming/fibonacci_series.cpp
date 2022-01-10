4203150103dd7355ea23c4173b71695fbb0d945d
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-11 01:17:39 +0530

    Update fibonacci_series.cpp

--- Luv Babbar dsa/Dynamic Programming/fibonacci_series.cpp
+++ Luv Babbar dsa/Dynamic Programming/fibonacci_series.cpp
@@ -27,3 +27,23 @@ int main()
     cout<<fib(n,dp);
     return 0;
 }
+
+// tabular form dp Approach
+// Bottom Up dp 
+// tc :O(N)
+// sc:O(1)
+int main()
+{
+    int n;
+    cin>>n;
+    int prev2=0;
+    int prev1=1;
+    for(int i=2;i<=n;i++)
+    {
+        int curri=prev1+prev2;
+        prev2=prev1;
+        prev1=curri;
+    }
+    cout<<prev1<<endl;
+    
+}
