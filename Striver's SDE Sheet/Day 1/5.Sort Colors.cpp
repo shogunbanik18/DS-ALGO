d31ba5c038247245a3298eb9dea85ccac05fe784
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-10 14:43:58 +0530

    Update 5.Sort Colors.cpp

--- Striver's SDE Sheet/Day 1/5.Sort Colors.cpp
+++ Striver's SDE Sheet/Day 1/5.Sort Colors.cpp
@@ -1,11 +1,57 @@
 class Solution {
 public:
-    // void sortColors(vector<int>& nums)
-    // {
-    //     sort(nums.begin(),nums.end());
-    // }
+    // Approach 1 :
+    // tc : O(NlogN)
+    // sc: O(1)
+    void sortColors(vector<int>& nums)
+    {
+        sort(nums.begin(),nums.end());
+    }
+    
+    // Approach 2
+    // tc: O(N) + O(N)
+    // sc: O(N) for vector v
+    void sortColors(vector<int>& nums)
+    {
+        int count1=0,count2=0,count3=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+            {
+                count1++;
+            }
+            else if(nums[i]==1)
+            {
+                count2++;
+            }
+            else
+            {
+                count3++;
+            }
+        }
+        vector<int>v;
+        while(count1--)
+        {
+            v.push_back(0);
+        }
+        while(count2--)
+        {
+            v.push_back(1);
+        }
+        while(count3--)
+        {
+            v.push_back(2);
+        }
+        for(int i=0;i<v.size();i++)
+        {
+            nums[i]=v[i];
+        }
+    }    
     
     // using sliding window approach 
+    // Dutch National flag algorithm 
+    // tc: O(N)
+    // sc: O(1)
     void swap1(vector<int>&nums,int a,int b)
     {
         int temp=nums[a];
@@ -15,25 +61,25 @@ public:
     void sortColors(vector<int>& nums)
     {
         int n=nums.size();
-        int i=0;
+        int mid=0;
         int l=0;
         int r=n-1;
-        while(i<=r)
+        while(mid<=r)
         {
-            if(nums[i]<1)
+            if(nums[mid]==0)
             {
-                swap1(nums,i,l);
-                i++;
+                swap1(nums,mid,l);
+                mid++;
                 l++;
             }
-            else if(nums[i]>1)
+            else if(nums[mid]==2)
             {
-                swap1(nums,i,r);
+                swap1(nums,mid,r);
                 r--;
             }
             else
             {
-                i++;
+                mid++;
             }
         }
     }
