a8e0a9203fa09a51d8877658f1546094b5e776f2
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-14 14:07:11 +0530

    Create Range Sum.cpp

---
+++ Luv Babbar dsa/Binary Search Tree/Range Sum.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumofRange;
+  
+    void dfs(TreeNode* root, int low, int high)
+    {
+        // base case 
+        if(root==NULL)
+        {
+            return ;
+        }
+        
+        if(root->val>=low and root->val<=high)
+        {
+            sumofRange+=root->val;
+        }
+        if(root->val>low)
+        {
+            dfs(root->left,low,high);
+        }
+        if(root->val<high)
+        {
+            dfs(root->right,low,high);
+        }
+    }
+    
+    int rangeSumBST(TreeNode* root, int low, int high) 
+    {
+        
+        int sum=0;
+        dfs(root,low,high);
+        return sumofRange;
+    }
+};
