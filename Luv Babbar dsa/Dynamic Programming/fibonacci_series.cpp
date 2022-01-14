d93fc8b4e37674653e9eedafe7e805b29b62d49c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-15 01:50:27 +0530

    Update fibonacci_series.cpp

--- Luv Babbar dsa/Dynamic Programming/fibonacci_series.cpp
+++ Luv Babbar dsa/Dynamic Programming/fibonacci_series.cpp
@@ -47,3 +47,49 @@ int main()
     cout<<prev1<<endl;
     
 }
+
+
+
+// Leetcode 
+
+// class Solution {
+// public:
+    // using recursion     
+    // int fib(int n)
+    // {
+    //      if(n==0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(n==1)
+    //     {
+    //         return 1;
+    //     }
+    //     return fib(n-1)+fib(n-2);
+    // }
+    
+    // using memeoisation 
+//     int check(int n,vector<int>&dp)
+//     {    
+//          if(n==0)
+//         {
+//             return 0;
+//         }
+//         if(n==1)
+//         {
+//             return 1;
+//         }
+//          if(dp[n]!=-1)
+//          {
+//              return dp[n];
+//          }
+//         return dp[n]=check(n-1,dp)+check(n-2,dp);
+//     }
+    
+//     int fib(int n)
+//     {
+//         vector<int>dp(n+1,-1);
+//         return check(n,dp);
+//     }
+// };
+
