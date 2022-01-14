2e62ae23f1d260ac5102c15194ab0d66aee88580
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-15 01:25:33 +0530

    Create 2 .Minimum number of platforms required for a railway.cpp

---
+++ Striver's SDE Sheet/Day 8/2 .Minimum number of platforms required for a railway.cpp
@@ -0,0 +1,39 @@
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+//     tc:O(nlogn *2) +o(2n)
+//     sc: o(1)
+    int findPlatform(int arr[], int dep[], int n)
+    {
+        // using greedy approach 
+        
+        // sorting +two pointer approach 
+        sort(arr,arr+n);
+        sort(dep,dep+n);
+        
+        int platform=1;
+        int result=1;
+        int i=1,j=0;
+        
+        while(i<n and j<n)
+        {
+            if(arr[i]<=dep[j])
+            {
+                platform++;
+                i++;
+            }
+            else if(arr[i]>dep[j])
+            {
+                platform--;
+                j++;
+            }
+            if(platform>result)
+            {
+                result=platform;
+            }
+        }
+        return result;
+    	// Your code here
+    }
+};
