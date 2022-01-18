e8c1f652e073f5ca46ca0d20d1fa711f5e1d4d4a
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-18 17:08:54 +0530

    Update 3 .Merge 2 sorted arrays .cpp

--- Striver's SDE Sheet/Day 2/3 .Merge 2 sorted arrays .cpp
+++ Striver's SDE Sheet/Day 2/3 .Merge 2 sorted arrays .cpp
@@ -50,3 +50,33 @@
 //                j--;
 //            }
 //         }  
+
+
+// BRUTE FORCE 
+// using extra space 
+    // using extra space 
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+//     {
+//         vector<int>v;
+//         for(int i=0;i<m;i++)
+//         {
+//             if(nums1[i]>0)
+//             {
+//                 v.push_back(nums1[i]);
+//             }
+//         }
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             v.push_back(nums2[i]);
+//         }
+        
+//         sort(v.begin(),v.end());
+//         for(int i=0;i<v.size();i++)
+//         {
+//             nums1[i]=v[i];
+//         }
+//     }
+
+// optimised method gap method 
+// using (N*LogN)
