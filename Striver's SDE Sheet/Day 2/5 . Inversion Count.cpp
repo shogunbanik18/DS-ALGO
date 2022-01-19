dfa3d96007414fbeae01bd4d80db00a1959b683c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-19 16:00:11 +0530

    Update 5 . Inversion Count.cpp

--- Striver's SDE Sheet/Day 2/5 . Inversion Count.cpp
+++ Striver's SDE Sheet/Day 2/5 . Inversion Count.cpp
@@ -85,3 +85,72 @@ int main()
     cout<<"No. of inversions are "<<ans<<endl;
     return 0;
 }
+
+
+// gfg 
+class Solution{
+  public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
+    // using merge sort
+    
+      long long int merge(long long arr[],long long  l,long long  m,long long  r)
+     {
+         long long  n1=m-l+1;
+         long long  n2=r-m;
+         long long left[n1],right[n2];
+         for(long long  i=0;i<n1;i++)
+         {
+             left[i]=arr[i+l];
+         }
+         for(long long  i=0;i<n2;i++)
+         {
+             right[i]=arr[m+1+i];
+         }
+         long long  i=0,j=0,k=l;
+         long long  res=0;
+         while(i<n1 && j<n2)
+         {
+             if(left[i]<=right[j])
+             {
+                 arr[k++]=left[i++];
+             }
+             else
+             {
+                 arr[k++]=right[j++];
+                 res=res+(n1-i);
+             }
+         }
+         while(i<n1)
+         {
+             arr[k++]=left[i++];
+         }
+         while(j<n2)
+         {
+             arr[k++]=right[j++];
+         }
+         return res;
+     }
+    
+    long long int mergesort(long long arr[],long long  l,long long  r)
+    {
+        long long inv=0;
+        if(l<r)
+        {
+            long long mid=(l+r)/2;
+            inv+=mergesort(arr,l,mid);
+            inv+=mergesort(arr,mid+1,r);
+            inv+=merge(arr,l,mid,r);
+        }
+        return inv;
+    }
+    
+    long long int inversionCount(long long arr[], long long N)
+    {
+        long long  l=0;
+        long long  r=N-1;
+        return mergesort(arr,l,r);
+        // Your Code Here
+    }
+};
