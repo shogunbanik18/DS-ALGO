65ed6a5ea8616d16828f2446323fd4a55ef6eb04
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-20 21:29:13 +0530

    Update 2 . Pow(x, n).cpp

--- Striver's SDE Sheet/Day 3/2 . Pow(x, n).cpp
+++ Striver's SDE Sheet/Day 3/2 . Pow(x, n).cpp
@@ -32,25 +32,29 @@ public:
         return ans;
     }
     
-//   check Brute force 
+//   check Brute force
+    // check for integer range 
     // Brute force 
     // tc : o(n)
     // sc:o(1)
-//     double myPow(double x, int n)
+//   double mypow(double x,int n)
+// {
+//     double ans=1.0;
+//     long long nn=n;
+//     if(n<0)
 //     {
-//         double ans=1.0;
-//         long long nn=n;
-//         for(int i=0;i<nn;i++)
-//         {
-//             ans=ans*x;
-//         }
-        
-//         if(n<0)
-//         {
-//             ans=((double)(1.0))/((double)(ans));
-//         }
-//         return ans;
+//         nn=-1*nn;
 //     }
+//     for(int i=0;i<nn;i++)
+//     {
+//         ans=ans*x;
+//     }
+//     if(n<0)
+//     {
+//         ans=(double)1.0/(double)ans;
+//     }
+//     return ans;
+// }
     
     // using stl library 
     // double myPow(double x, int n)
@@ -59,3 +63,39 @@ public:
     //     return ans;
     // }
 };
+
+
+
+// brute force 
+// check for integer range 
+#include<bits/stdc++.h>
+using namespace std;
+
+// brute force 
+// tc:o(n)
+// sc:o(1)
+double mypow(double x,int n)
+{
+    double ans=1.0;
+    long long nn=n;
+    if(n<0)
+    {
+        nn=-1*nn;
+    }
+    for(int i=0;i<nn;i++)
+    {
+        ans=ans*x;
+    }
+    if(n<0)
+    {
+        ans=(double)1.0/(double)ans;
+    }
+    return ans;
+}
+
+int main()
+{
+    int x,n;
+    cin>>x>>n;
+    cout<<mypow(x,n)<<endl;
+}
