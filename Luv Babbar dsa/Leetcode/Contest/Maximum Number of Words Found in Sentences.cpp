63b29b82b7a8744251b9ff7bcc5cdcc798a8625e
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-22 14:05:32 +0530

    Update Maximum Number of Words Found in Sentences.cpp

--- Luv Babbar dsa/Leetcode/Contest/Maximum Number of Words Found in Sentences.cpp
+++ Luv Babbar dsa/Leetcode/Contest/Maximum Number of Words Found in Sentences.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
+    // int mostWordsFound(vector<string>& sentences) 
+    // {
+    //     int maxcount=0;
+    //     for(auto x:sentences)
+    //     {
+    //         int count=0;
+    //         for(auto ch:x)
+    //         {
+    //             if(ch==' ')
+    //             {
+    //                 count++;
+    //             }
+    //         }
+    //         maxcount=max(maxcount,count+1);
+    //     }
+    //     return maxcount;
+    // }
+    
+    // using stl 
     int mostWordsFound(vector<string>& sentences) 
     {
         int maxcount=0;
         for(auto x:sentences)
         {
-            int count=0;
-            for(auto ch:x)
-            {
-                if(ch==' ')
-                {
-                    count++;
-                }
-            }
-            maxcount=max(maxcount,count+1);
+            int cnt =count(x.begin(),x.end(),' ')+1;
+            maxcount=max(maxcount,cnt);
         }
         return maxcount;
     }
