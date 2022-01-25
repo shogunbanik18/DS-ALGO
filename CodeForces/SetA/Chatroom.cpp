13bec3853f83c57075659d6d675d18849879499d
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-25 18:28:36 +0530

    Create Chatroom.cpp

---
+++ CodeForces/SetA/Chatroom.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    string k="hello";
+    int j=0,count=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]==k[j])
+        {
+            j++;
+            count++;
+            if(count==5)
+            {
+                break;
+            }
+        }
+    }
+    if(count==5)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
