550f5814e034064eb755b652d265706f31719bae
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-01-28 23:47:28 +0530

    Update 3 .Merge two sorted Linked List (use method used in mergeSort).cpp

--- Striver's SDE Sheet/Day 5/3 .Merge two sorted Linked List (use method used in mergeSort).cpp
+++ Striver's SDE Sheet/Day 5/3 .Merge two sorted Linked List (use method used in mergeSort).cpp
@@ -11,6 +11,38 @@
 class Solution {
 public:
     
+    // iterative approach 
+    // tc:o(n1+n2)
+    // sc:o(1)
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        if(list1==NULL)
+        {
+            return list2;
+        }
+        if(list2==NULL)
+        {
+            return list1;
+        }
+        if(list1->val>list2->val)
+        {
+            swap(list1,list2);
+        }
+        ListNode*res=list1;
+        while(list1!=NULL and list2!=NULL)
+        {
+            ListNode*temp=NULL;
+            while(list1!=NULL and list1->val<=list2->val )
+            {
+                temp=list1;
+                list1=list1->next;
+            }
+            temp->next=list2;
+            swap(list1,list2);
+        }
+        return res;
+    }
+    
     // tc:O(n1)+O(n2)
     // sc:o(n1+n2)
     // using iterative approach 
