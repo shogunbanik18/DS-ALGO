895c6fdd1c2addd5c69154bf48040eb6dcecf327
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-02 14:12:23 +0530

    Update Ninja's Friend_3D DP (Cherry Pick _Leetcode).cpp

--- Luv Babbar dsa/Dynamic Programming/Ninja's Friend_3D DP (Cherry Pick _Leetcode).cpp
+++ Luv Babbar dsa/Dynamic Programming/Ninja's Friend_3D DP (Cherry Pick _Leetcode).cpp
@@ -1,15 +1,13 @@
-
-// // using Recursion 
-// // tc: (3^N +3^N)
-// // sc:O(1) auxiliary stack space
+// // using recursion 
+// tc: 3^n *3^n 
+// sc:o(n) stack space 
 int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid)
-{ 
-//     outof bound cases 
-    if((j1<0 or j1>=c) or (j2<0 or j2>=c))
+{
+//     out of bound cases
+    if(j1<0 or j1>=c or j2<0 or j2>=c)
     {
         return -1e8;
     }
-//     destination case 
     if(i==r-1)
     {
         if(j1==j2)
@@ -21,8 +19,7 @@ int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid)
             return grid[i][j1]+grid[i][j2];
         }
     }
-    
-//     explore all possible paths 9 paths here
+//     explore all the stuffs
     int maxi=-1e8;
     for(int dj1=-1;dj1<=1;dj1++)
     {
@@ -31,7 +28,7 @@ int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid)
             int value=0;
             if(j1==j2)
             {
-                value+=grid[i][j1];
+            	value+=grid[i][j1];   
             }
             else
             {
@@ -49,15 +46,15 @@ int maximumChocolates(int r, int c, vector<vector<int>> &grid)
     // Write your code here.
 }
 
-
-// using Memoisation  
-// tc: o(N*M*M)
-// sc:O(n+m+m)+O(1) auxiliary stack space
+// // using memoisation 
+// // // using recursion 
+// // tc: o(n*m*m)
+// // sc:o(n) +o(n*m*m)stack space 
 int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid,
-     vector<vector<vector<int>>>&dp)
-{ 
-//     outof bound cases 
-    if((j1<0 or j1>=c) or (j2<0 or j2>=c))
+      vector<vector<vector<int>>>&dp)
+{
+//     out of bound cases
+    if(j1<0 or j1>=c or j2<0 or j2>=c)
     {
         return -1e8;
     }
@@ -77,7 +74,7 @@ int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid,
     {
         return dp[i][j1][j2];
     }
-//     explore all possible paths 9 paths here
+//     explore all the stuffs
     int maxi=-1e8;
     for(int dj1=-1;dj1<=1;dj1++)
     {
@@ -86,7 +83,7 @@ int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid,
             int value=0;
             if(j1==j2)
             {
-                value+=grid[i][j1];
+            	value+=grid[i][j1];   
             }
             else
             {
@@ -98,11 +95,137 @@ int f(int i,int j1,int j2,int r,int c,vector<vector<int>> &grid,
     }
     return dp[i][j1][j2]=maxi;
 }
+
 int maximumChocolates(int r, int c, vector<vector<int>> &grid) 
 {
-//     declaring a 3d dp 
-//     int dp[n][m][m]
     vector<vector<vector<int>>>dp(r,vector<vector<int>>(c,vector<int>(c,-1)));
     return f(0,0,c-1,r,c,grid,dp);
+    // Write your code here.
+}
+
+// using bottom up dp 
+// tc: o(n*m*m)
+// sc:o(n*m*m) 
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) 
+{
+    vector<vector<vector<int>>>dp(r,vector<vector<int>>(c,vector<int>(c,0)));
+    for(int j1=0;j1<c;j1++)
+    {
+        for(int j2=0;j2<c;j2++)
+        {
+            if(j1==j2)
+            {
+                dp[r-1][j1][j2]=grid[r-1][j1];
+            }
+            else
+            {
+                dp[r-1][j1][j2]=grid[r-1][j1]+grid[r-1][j2];
+            }
+        }
+    }
+    
+    for(int i=r-2;i>=0;i--)
+    {
+        for(int j1=0;j1<c;j1++)
+        {
+            for(int j2=0;j2<c;j2++)
+            {
+                 int maxi=-1e8;
+                for(int dj1=-1;dj1<=1;dj1++)
+                {
+                    for(int dj2=-1;dj2<=1;dj2++)
+                    {
+                        int value=0;
+                        if(j1==j2)
+                        {
+                            value+=grid[i][j1];   
+                        }
+                        else
+                        {
+                            value+=grid[i][j1]+grid[i][j2];
+                        }
+                        if(j1+dj1>=0 and j1+dj1<c and j2+dj2>=0 
+                           and j2+dj2<c)
+                        {
+                            value+=dp[i+1][j1+dj1][j2+dj2];
+                        }
+                        else
+                        {
+                            value+=-1e8;
+                        }
+                        maxi=max(maxi,value);
+                    }
+                }
+                dp[i][j1][j2]=maxi;
+            }
+        }
+    }
+    return dp[0][0][c-1];
+    // Write your code here.
+}
+
+
+// using space optimisation dp 
+// tc: o(n*m*m)
+// sc:o(m*m*) 
+int maximumChocolates(int r, int c, vector<vector<int>> &grid) 
+{
+//    int front[m][m];
+//    int curr[m][m];
+    vector<vector<int>>front(c,vector<int>(c,0));
+    vector<vector<int>>curr(c,vector<int>(c,0));
+    for(int j1=0;j1<c;j1++)
+    {
+        for(int j2=0;j2<c;j2++)
+        {
+            if(j1==j2)
+            {
+                front[j1][j2]=grid[r-1][j1];
+            }
+            else
+            {
+                front[j1][j2]=grid[r-1][j1]+grid[r-1][j2];
+            }
+        }
+    }
+    
+    for(int i=r-2;i>=0;i--)
+    {
+        for(int j1=0;j1<c;j1++)
+        {
+            for(int j2=0;j2<c;j2++)
+            {
+                 int maxi=-1e8;
+                for(int dj1=-1;dj1<=1;dj1++)
+                {
+                    for(int dj2=-1;dj2<=1;dj2++)
+                    {
+                        int value=0;
+                        if(j1==j2)
+                        {
+                            value+=grid[i][j1];   
+                        }
+                        else
+                        {
+                            value+=grid[i][j1]+grid[i][j2];
+                        }
+                        if(j1+dj1>=0 and j1+dj1<c and j2+dj2>=0 
+                           and j2+dj2<c)
+                        {
+                            value+=front[j1+dj1][j2+dj2];
+                        }
+                        else
+                        {
+                            value+=-1e8;
+                        }
+                        maxi=max(maxi,value);
+                    }
+                }
+                curr[j1][j2]=maxi;
+            }
+        }
+        front=curr;
+    }
+    return front[0][c-1];
     // Write your code here.
 }
