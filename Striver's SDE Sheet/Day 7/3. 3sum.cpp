ac5840d35ef32b430366a306f935bcec92091636
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-03 14:18:47 +0530

    Update 3. 3sum.cpp

--- Striver's SDE Sheet/Day 7/3. 3sum.cpp
+++ Striver's SDE Sheet/Day 7/3. 3sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    // brute force 
-    // tc:o(nlogn)+o(n*n*n)
-    // sc:o(1)+o(n) for set
-    vector<vector<int>> threeSum(vector<int>& nums) 
+//     Brute force 
+//     tc:o(n^3logm)
+//     sc:o(m) unordered set space 
+    vector<vector<int>> threeSum(vector<int>& nums)
     {
+        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        int n=nums.size();
         set<vector<int>>s;
-        vector<vector<int>>v;
+        int n=nums.size();
         for(int i=0;i<n-2;i++)
         {
             for(int j=i+1;j<n-1;j++)
@@ -22,14 +22,102 @@ public:
                         temp.push_back(nums[j]);
                         temp.push_back(nums[k]);
                         s.insert(temp);
-                    }
+                    } 
                 }
             }
         }
         for(auto x:s)
         {
-            v.push_back(x);
+            ans.push_back(x);
+        }
+        return ans;
+    }
+    
+    // using hashing based approach 
+    // check 
+    // vector<vector<int>> threeSum(vector<int>& nums)
+    // {
+    //     int n=nums.size();
+    //     vector<vector<int>>v;
+    //     unordered_map<int,int>mp;
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         mp[nums[i]]++;
+    //     }
+    //     for(int i=0;i<n-2;i++)
+    //     {
+    //         mp[nums[i]]--;
+    //         for(int j=i+1;j<n;j++)
+    //         {
+    //             mp[nums[j]]--;
+    //             int target =-(nums[i]+nums[j]);
+    //             if(mp.find(target)!=mp.end())
+    //             {
+    //                 vector<int>temp;
+    //                 temp.push_back(nums[i]);
+    //                 temp.push_back(nums[j]);
+    //                 temp.push_back(nums[target]);
+    //                 v.push_back(temp);
+    //             }
+    //             mp[nums[j]]++;
+    //         }
+    //         mp[nums[i]]++;
+    //     }
+    //     return v;
+    // } 
+    
+    // Optimised Approach 
+    // avod duplicates +sorting +two pointer approach 
+    // tc:o(n*n)
+    // sc:o(m) auxiliary stack spaceis o(1)
+    // case of avoiding duplicates check ery adjacent elements 
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+        vector<vector<int>>v;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-2;i++)
+        {
+            if(i==0 or (i>0 and nums[i]!=nums[i-1]))
+            {
+                int target=-nums[i];
+                int j=i+1;
+                int k=n-1;
+                while(j<k)
+                {
+                    if(nums[j]+nums[k]==target)
+                    {
+                        vector<int>temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[j]);
+                        temp.push_back(nums[k]);
+                        v.push_back(temp);
+                        // Avoiding Duplicates 
+                        while(j<k and nums[j]==nums[j+1])
+                        {
+                            j++;
+                        }
+                        while(j<k and nums[k]==nums[k-1])
+                        {
+                            k--;
+                        }
+                        j++;
+                        k--;
+                    }
+                    else if(nums[j]+nums[k]<target)
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                       k--;
+                    }
+                }
+            }
         }
         return v;
-    }
+    } 
 };
+
+
+
