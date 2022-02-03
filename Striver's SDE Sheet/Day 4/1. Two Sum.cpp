a14c2885286f2a5366da488944501c48bf7db2cf
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-03 14:44:57 +0530

    Update 1. Two Sum.cpp

--- Striver's SDE Sheet/Day 4/1. Two Sum.cpp
+++ Striver's SDE Sheet/Day 4/1. Two Sum.cpp
@@ -76,3 +76,81 @@ public:
      //        return ans;
      //    }
 };
+
+
+
+
+// leetcode 
+
+class Solution {
+public:
+    // brute force 
+    // tc:o(n^2)
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int>ans;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    // using hashmap based approach 
+    // tc:o(n)
+    // sc:o(n)
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int>v;
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+            {
+                v.push_back(i);
+                v.push_back(mp[target-nums[i]]);
+            }
+            mp[nums[i]]=i;
+        }
+        return v;
+    }
+    
+    // using 2 pointer approach 
+    // sorting changes the index so sorting s not preferred 
+    // and hence two pointer should not be used here
+    // vector<int> twoSum(vector<int>& nums, int target) 
+    // {
+    //     vector<int>v;
+    //     sort(nums.begin(),nums.end());
+    //     int n=nums.size();
+    //     int low=0;
+    //     int high=n-1;
+    //     while(low<high)
+    //     {
+    //         if(nums[low]+nums[high]==target)
+    //         {
+    //             v.push_back(low);
+    //             v.push_back(high);
+    //             break;
+    //         }
+    //         else if(nums[low]+nums[high]<target)
+    //         {
+    //             low++;
+    //         }
+    //         else
+    //         {
+    //             high--;
+    //         }
+    //     }
+    //     return v;
+    // }
+};
