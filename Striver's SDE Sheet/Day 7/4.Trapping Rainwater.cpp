62482146fa2e1c4a0fb14af37f78f0d33cdb3a90
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-04 21:00:05 +0530

    Update 4.Trapping Rainwater.cpp

--- Striver's SDE Sheet/Day 7/4.Trapping Rainwater.cpp
+++ Striver's SDE Sheet/Day 7/4.Trapping Rainwater.cpp
@@ -1,8 +1,36 @@
-  // using the concept of array preprocessing 
-    // tc:o(n)+o(n)+o(n)
-    // sc:o(1)
 class Solution {
 public:
+    // Using Brute force 
+    // tc:o(n*n)
+    // sc:o(1)
+    int trap(vector<int>& height) 
+    {
+        int n=height.size();
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int j=i;
+            int leftmax=0,rightmax=0;
+            while(j>=0)
+            {
+                leftmax=max(leftmax,height[j]);
+                j--;
+            }
+            
+            j=i;
+            while(j<n)
+            {
+                rightmax=max(rightmax,height[j]);
+                j++;
+            }
+            ans+=min(leftmax,rightmax)-height[i];
+        }
+        return ans;
+    }
+    
+    // using the concept of array preprocessing 
+    // tc:o(n)+o(n)+o(n)
+    // sc:o(n)+o(n)
     int trap(vector<int>& height) 
     {
         int n=height.size();
@@ -29,4 +57,44 @@ public:
         }
         return ans;
     }
+
+    // using 2 pointer approach 
+    // tc:o(n)
+    // sc:o(1)
+     int trap(vector<int>& height)
+     {
+         int n=height.size();
+         int l=0;
+         int r=n-1;
+         int leftmax=0,rightmax=0;
+         int res=0;
+         while(l<=r)
+         {
+            if(height[l]<=height[r])
+            {
+                if(height[l]>=leftmax)
+                {
+                    leftmax=height[l];
+                }
+                else
+                {
+                    res+=leftmax-height[l];    
+                }
+                l++;
+            }
+            else
+               {
+                   if(height[r]>=rightmax)
+                   {
+                       rightmax=height[r];
+                   }
+                   else
+                   {
+                       res+=rightmax-height[r];
+                   }
+                   r--;
+               }
+         }
+          return res;
+     }
 };
