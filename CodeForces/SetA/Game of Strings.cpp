49dd371fa9bca3a884b04365c4d905a2789aaffd
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-09 00:02:50 +0530

    Create Game of Strings.cpp

---
+++ CodeForces/SetA/Game of Strings.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int a=n*m;
+    if(n>m)
+    {
+        swap(n,m);
+    }
+    if(n%2==0)
+    {
+        cout<<"Malvika"<<endl;
+    }
+    else
+    {
+        cout<<"Akshat"<<endl;
+    }
+}
