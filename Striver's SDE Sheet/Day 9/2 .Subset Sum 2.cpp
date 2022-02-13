f95377c5ab843a061f2428cd26457884637ac814
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-13 22:16:37 +0530

    Create 2 .Subset Sum 2.cpp

---
+++ Striver's SDE Sheet/Day 9/2 .Subset Sum 2.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    // tc:o(2^n) +o(nlogn)
+    // sc:o(n)
+    void f(int ind,vector<int>&nums,vector<int>&ds,set<vector<int>>&res)
+    {
+        int n=nums.size();
+        if(ind==n)
+        {
+            sort(ds.begin(),ds.end());
+            res.insert(ds);
+            return;
+        }
+        ds.push_back(nums[ind]);
+        f(ind+1,nums,ds,res);
+        ds.pop_back();
+        f(ind+1,nums,ds,res);
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        set<vector<int>>res;
+        vector<int>ds;
+        f(0,nums,ds,res);
+        // for(auto x: res)
+        // {
+        //     ans.push_back(res);
+        // }
+        for(auto x =res.begin();x!=res.end();x++)
+        {
+            ans.push_back(*x);
+        }
+        return ans;
+    }
+    
+    // tc: O(2^n)
+    // sc: O(2^n * k)
+    void f(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans)
+    {
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++)
+        {
+            if(i!=ind and nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            ds.push_back(nums[i]);
+            f(i+1,nums,ds,ans);
+            ds.pop_back();  
+        }
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        sort(nums.begin(),nums.end());
+        f(0,nums,ds,ans);
+        return ans;
+    }
+};
