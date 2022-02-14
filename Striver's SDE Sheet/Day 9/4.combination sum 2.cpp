8e1bdcce852097b5b78f2f37724e1073f42664c1
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-14 17:10:25 +0530

    Update 4.combination sum 2.cpp

--- Striver's SDE Sheet/Day 9/4.combination sum 2.cpp
+++ Striver's SDE Sheet/Day 9/4.combination sum 2.cpp
@@ -1,38 +1,81 @@
-// tc :2^n *k 
-// sc: k*x
+// Leetcode 
 class Solution {
 public:
-    void findcombination(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds)
+    // using optimised approach 
+    // using reursion with skipping duplicates 
+    // 2nd method of recursion 
+    // tc:o(2^n)*k
+    // sc:o(k*x)
+    void f(int ind,int target, vector<int>&arr, vector<int>&ds, vector<vector<int>>&ans)
     {
+        int n=arr.size();
         if(target==0)
         {
             ans.push_back(ds);
-            return;
         }
         
-        for(int i=ind;i<arr.size();i++)
+        for(int i=ind;i<n;i++)
         {
-            if(i>ind and arr[i]==arr[i-1])
-            {
-                continue;
-            }
             if(arr[i]>target)
             {
                 break;
             }
+            if(i>ind and arr[i]==arr[i-1])
+            {
+                continue;
+            }
             ds.push_back(arr[i]);
-            findcombination(i+1,target-arr[i],arr,ans,ds);
+            f(i+1,target-arr[i],arr,ds,ans);
             ds.pop_back();
         }
+    }
+    
+     vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
+     {
+         sort(candidates.begin(),candidates.end());
+         vector<vector<int>>ans;
+         vector<int>ds;
+         f(0,target,candidates,ds,ans);
+         return ans;
+     }
+    
+//     // Brute Force 
+//     // using Recursion  + set 
+    // tc: o(2^n) *klogn
+    // sc: o(n)
+    void f(int ind,int target,vector<int>&arr,vector<int>&ds,set<vector<int>>&k)
+    {
+        if(ind==arr.size())
+        {
+            if(target==0)
+            {
+                // sort(ds.begin(),ds.end());
+                k.insert(ds);
+                // ans.push_back(ds);
+            }
+            return;
+        }
+        if(arr[ind]<=target)
+        {
+            ds.push_back(arr[ind]);
+            f(ind+1,target-arr[ind],arr,ds,k);
+            ds.pop_back();
+        }
+        f(ind+1,target,arr,ds,k);
     }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
     {
         sort(candidates.begin(),candidates.end());
         vector<vector<int>>ans;
+        set<vector<int>>k;
         vector<int>ds;
-        findcombination(0,target,candidates,ans,ds);
-        return ans;       
+        f(0,target,candidates,ds,k);
+        for(auto it=k.begin();it!=k.end();it++)
+        {
+            ans.push_back(*it);
+        }
+        return ans;
     }
 };
 
