12e6fe02a1366763a6972266054df3a72acf08b2
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-15 21:57:57 +0530

    Update 2.Nqueen.cpp

--- Striver's SDE Sheet/Day 10/2.Nqueen.cpp
+++ Striver's SDE Sheet/Day 10/2.Nqueen.cpp
@@ -1,10 +1,10 @@
-// Leetcode 
 class Solution {
-public:
-//     using extra space     
-    bool issafe(int row,int col,vector<string>board,int n)
+public:   
+//     brute force 
+//     tc:o(n^3)
+//     sc:o(n^2)
+    bool issafe(int row,int col,vector<string>&board,int n)
     {
-        // upper diagonal check 
         int duprow=row;
         int dupcol=col;
         while(row>=0 and col>=0)
@@ -17,9 +17,9 @@ public:
             col--;
         }
         
-        col=dupcol;
         row=duprow;
-        while( col>=0)
+        col=dupcol;
+        while(col>=0)
         {
             if(board[row][col]=='Q')
             {
@@ -28,10 +28,9 @@ public:
             col--;
         }
         
-        // lower diagonal 
-        col=dupcol;
         row=duprow;
-        while( row<n and col>=0)
+        col=dupcol;
+        while(row<n and col>=0)
         {
             if(board[row][col]=='Q')
             {
@@ -45,10 +44,11 @@ public:
     
     void solve(int col,vector<string>&board,vector<vector<string>>&ans,int n)
     {
-        if (col == n) {
-        ans.push_back(board);
-        return;
-      }
+        if(col==n)
+        {
+            ans.push_back(board);
+            return ;
+        }
         
         for(int row=0;row<n;row++)
         {
@@ -65,44 +65,41 @@ public:
     {
         vector<vector<string>>ans;
         vector<string>board(n);
-        string s(n,'.');
-        
+        string s(n, '.');
         for(int i=0;i<n;i++)
         {
-            board[i]=s;  
+            board[i]=s;
         }
         solve(0,board,ans,n);
         return ans;
     }
-};
-
     
-// Optimized Solution     
-// using haspmap 
-class Solution {
-public:
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans,vector<int>&leftrow,
-               vector<int>&upperdiag,vector<int>&lowerdiag,int n)
+    // using optimised approach 
+    // hashing
+    // tc: O( N^2 )
+    // sc: O( N^2 )
+    void solve(int col,vector<string>&board,vector<vector<string>>&ans, vector<int>&leftrow, vector<int>&upperdiagonal, vector<int>&lowerdiagonal,int n)
     {
-       if (col == n) {
-        ans.push_back(board);
-        return;
-      }
+        if(col==n)
+        {
+            ans.push_back(board);
+            return ;
+        }
         
         for(int row=0;row<n;row++)
         {
-              if(leftrow[row]==0 and upperdiag[n-1+col-row]==0 and lowerdiag[col+row]==0)
-              {
-                  board[row][col]='Q';
-                  leftrow[row]=1;
-                  lowerdiag[row+col]=1;
-                  upperdiag[n-1+col-row]=1;
-                  solve(col+1,board,ans,leftrow,upperdiag,lowerdiag,n);
-                  board[row][col]='.';
-                  leftrow[row]=0;
-                  lowerdiag[row+col]=0;
-                  upperdiag[n-1+col-row]=0;
-              }
+            if(leftrow[row]==0 and lowerdiagonal[row+col]==0 and upperdiagonal[n-1+col-row]==0)
+            {
+                board[row][col]='Q';
+                leftrow[row]=1;
+                lowerdiagonal[row+col]=1;
+                upperdiagonal[n-1+col-row]=1;
+                solve(col+1,board,ans,leftrow,upperdiagonal,lowerdiagonal,n);
+                board[row][col]='.';
+                leftrow[row]=0;
+                lowerdiagonal[row+col]=0;
+                upperdiagonal[n-1+col-row]=0;
+            }
         }
     }
     
@@ -110,22 +107,13 @@ public:
     {
         vector<vector<string>>ans;
         vector<string>board(n);
-        string s(n,'.');
-        
+        string s(n, '.');
         for(int i=0;i<n;i++)
         {
-            board[i]=s;  
+            board[i]=s;
         }
-        
-        vector<int>leftrow(n,0);
-        vector<int>upperdiag(2*n-1,0);
-        vector<int>lowerdiag(2*n-1,0);
-        solve(0,board,ans,leftrow,upperdiag,lowerdiag,n);
+        vector<int>leftrow(n,0),upperdiagonal((2*n-1),0),lowerdiagonal((2*n-1),0);
+        solve(0,board,ans,leftrow,upperdiagonal,lowerdiagonal,n);
         return ans;
     }
 };
-
-    
-    
-    
-    
