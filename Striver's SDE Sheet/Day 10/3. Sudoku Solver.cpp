26ef87af5ca4eab723bf43fda11856f54c74fcb8
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-16 10:59:36 +0530

    Update 3. Sudoku Solver.cpp

--- Striver's SDE Sheet/Day 10/3. Sudoku Solver.cpp
+++ Striver's SDE Sheet/Day 10/3. Sudoku Solver.cpp
@@ -1,34 +1,41 @@
-// Leetcode
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board){
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++)
-            {
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++)
-                    {
-                        if(isvalid(board, i, j, c))
-                        {
-                            board[i][j] = c; 
-                            
-                            if(solve(board))
-                                return true; 
-                            else
-                                board[i][j] = '.'; 
-                        }
-                    }
-                    
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+//   tc: O(9(n ^ 2)), in the worst case, for each cell in the n2 board, we have 9 possible numbers.
+
+//   sc: O(1), since we are refilling the given board itself, there is no extra space required, so constant space complexity.
     
-    bool isvalid(vector<vector<char>>&board,int row,int col,char c)
+     bool solve(vector<vector<char>>& board)
+     {
+         for(int i=0;i<board.size();i++)
+         {
+             for(int j=0;j<board[0].size();j++)
+             {
+                 if(board[i][j]=='.')
+                 {
+                     for(char c='1';c<='9';c++)
+                     {
+                         if(isvalid(board,i,j,c))
+                         {
+                             board[i][j]=c;
+                             if(solve(board)==true)
+                             {
+                                 return true;
+                             }
+                             else
+                             {
+                                    board[i][j]='.';
+                             }
+                         }
+                     }
+                    return false;
+                 }  
+             }
+         }
+         return true;
+     }
+    
+    bool isvalid(vector<vector<char>>& board,int row,int col,int c)
     {
-        // check for row,col and grid
         for(int i=0;i<9;i++)
         {
             if(board[i][col]==c)
@@ -40,8 +47,8 @@ public:
             {
                 return false;
             }
-            
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+            // using the concet of divide and modulo for matrix
+            if(board[(row/3)*3 + i/3][(col/3)*3+i%3]==c)
             {
                 return false;
             }
@@ -49,9 +56,8 @@ public:
         return true;
     }
     
-    void solveSudoku(vector<vector<char>>& board) 
+    void solveSudoku(vector<vector<char>>& board)
     {
-        solve(board);
+        solve(board);   
     }
 };
-    
