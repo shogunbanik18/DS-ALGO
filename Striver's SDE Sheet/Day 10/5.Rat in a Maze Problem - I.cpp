69c0486b9ea0280ba024109eb032ae3ba3ffdd40
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-16 12:36:27 +0530

    Update 5.Rat in a Maze Problem - I.cpp

--- Striver's SDE Sheet/Day 10/5.Rat in a Maze Problem - I.cpp
+++ Striver's SDE Sheet/Day 10/5.Rat in a Maze Problem - I.cpp
@@ -1,8 +1,8 @@
 class Solution{
     public:
-    // tc :4^(N*M)
-    // SC: auxiliary space  O(M*N)
-    void solve(int i,int j,vector<vector<int>>&a,int n,vector<string>&ans,string move,vector<vector<int>>&vis)
+    // tc : o(4 ^(m*n))
+    // sc: O(m*n) ,Maximum Depth of the recursion tree(auxiliary space).
+    void solve(int i,int j,vector<vector<int>> &a,int n,vector<string>&ans,string move,vector<vector<int>>&vis)
     {
         if(i==n-1 and j==n-1)
         {
@@ -10,7 +10,7 @@ class Solution{
             return;
         }
         
-        // downward direction 
+        // downwards direction 
         if(i+1<n and !vis[i+1][j] and a[i+1][j]==1)
         {
             vis[i][j]=1;
@@ -18,7 +18,7 @@ class Solution{
             vis[i][j]=0;
         }
         
-        // left direction 
+        // leftward direction 
         if(j-1>=0 and !vis[i][j-1] and a[i][j-1]==1)
         {
             vis[i][j]=1;
@@ -27,14 +27,14 @@ class Solution{
         }
         
         // Rightward direction 
-        if(j+1<n and !vis[i][j+1] and a[i][j+1]==1)
+        if(j+1<n and  !vis[i][j+1] and a[i][j+1]==1)
         {
             vis[i][j]=1;
             solve(i,j+1,a,n,ans,move+'R',vis);
             vis[i][j]=0;
         }
         
-        // Upward direction 
+        // upward direction 
         if(i-1>=0 and !vis[i-1][j] and a[i-1][j]==1)
         {
             vis[i][j]=1;
@@ -43,49 +43,60 @@ class Solution{
         }
     }
     
-    vector<string> findPath(vector<vector<int>> &m, int n) 
+    vector<string> findPath(vector<vector<int>> &m, int n)
     {
         vector<string>ans;
-        // declaring visted array
         vector<vector<int>>vis(n,vector<int>(n,0));
+        
         if(m[0][0]==1)
         {
             solve(0,0,m,n,ans,"",vis);
         }
         return ans;
-        // Your code goes here
     }
-    
-    
-// optimized 
+};
 
-    void solve(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans, string move, 
-    vector<vector<int>> &vis, int di[], int dj[]) 
+    
+// optimised approach 
+// instead of using 4 if statements using a for loop and a string ir ="DLRU"
+// using di and dj array 
+class Solution{
+    public:
+    // tc : o(4 ^(m*n))
+    // sc: O(m*n) ,Maximum Depth of the recursion tree(auxiliary space).
+    void solve(int i,int j,vector<vector<int>> &a,int n,vector<string>&ans,string move,vector<vector<int>>&vis,int di[],int dj[])
     {
-        if(i==n-1 && j==n-1) {
-            ans.push_back(move);
-            return; 
-        }
-        string dir = "DLRU"; 
-        for(int ind = 0; ind<4;ind++)
+        if(i==n-1 and j==n-1)
         {
-            int nexti = i + di[ind]; 
-            int nextj = j + dj[ind]; 
-            if(nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1) {
-                vis[i][j] = 1; 
-                solve(nexti, nextj, a, n, ans, move + dir[ind], vis, di, dj);
-                vis[i][j] = 0; 
-            }
+            ans.push_back(move);
+            return;
         }
+        
+        string dir ="DLRU";
+        for(int ind=0;ind<4;ind++)
+        {
+            int nexti= i+di[ind];
+            int nextj= j+dj[ind];
+            if(nexti>=0 and nextj>=0 and nexti<n and nextj<n and
+            !vis[nexti][nextj] and a[nexti][nextj]==1)
+            {
+                vis[i][j]=1;
+                solve(nexti,nextj,a,n,ans,move +dir[ind],vis,di,dj);
+                vis[i][j]=0;
+            }
+        }        
     }
-    // public:
-    vector<string> findPath(vector<vector<int>> &m, int n) 
+    
+    vector<string> findPath(vector<vector<int>> &m, int n)
     {
-        vector<string> ans;
-        vector<vector<int>> vis(n, vector<int> (n, 0)); 
-        int di[] = {+1, 0, 0, -1}; 
-        int dj[] = {0, -1, 1, 0}; 
-        if(m[0][0] == 1) solve(0,0,m,n, ans, "", vis, di, dj); 
-        return ans; 
+        vector<string>ans;
+        vector<vector<int>>vis(n,vector<int>(n,0));
+        int di[] ={1,0,0,-1};
+        int dj[] ={0,-1,1,0};
+        if(m[0][0]==1)
+        {
+            solve(0,0,m,n,ans,"",vis,di,dj);
+        }
+        return ans;
     }
 };
