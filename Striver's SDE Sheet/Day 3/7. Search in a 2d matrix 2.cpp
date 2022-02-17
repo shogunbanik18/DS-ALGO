07bb4d39e521fe740eeaac4f69120fa09792731c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-17 19:45:57 +0530

    Create 7. Search in a 2d matrix 2.cpp

---
+++ Striver's SDE Sheet/Day 3/7. Search in a 2d matrix 2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // using the efficient binary search 
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int i=0;
+        int j=n-1;
+        while(i<m and j>=0)
+        {
+            if(matrix[i][j]==target)
+            {
+                return true;
+            }
+            else if(matrix[i][j]>target)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return false;
+    }
+};
