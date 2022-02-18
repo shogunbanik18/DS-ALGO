b30df179684d2658b0f1a24f263c7b31278e3b01
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-18 14:18:11 +0530

    Create Devu, the Singer and Churu, the Joker.cpp

---
+++ CodeForces/SetA/Devu, the Singer and Churu, the Joker.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+#define PI 3.14
+#define _USE_MATH_DEFINES
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<ll, ll>
+#define pi pair<int, int>
+#define mp make_pair
+#define setbits(x) __builtin_popcountll(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define endl "\n"
+#define debug(x) cout << #x << " -> " << x << "\n";
+const ll mod = 1000000007;
+const ll MOD = 998244353;
+const ll inf = 1e18;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+	c_p_c();
+
+	// t1 +10 +t2 +10 +... =sum +(n-1)*10
+	int n, d;
+	cin >> n >> d;
+	int arr[n];
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		sum += arr[i];
+	}
+
+	int rem = d - sum;
+	if (sum + 10 * (n - 1) > d)
+	{
+		cout << "-1" << endl;
+	}
+	else
+	{
+		int ans = rem / 5;
+		cout << ans << endl;
+	}
+
+	return 0;
+
+}
