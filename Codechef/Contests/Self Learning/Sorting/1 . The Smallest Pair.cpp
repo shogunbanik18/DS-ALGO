6a5c2918880c1c6782bd1146d88f0121f61de30f
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-19 16:06:31 +0530

    Create 1 . The Smallest Pair.cpp

---
+++ Codechef/Contests/Self Learning/Sorting/1 . The Smallest Pair.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+#define PI 3.14
+#define _USE_MATH_DEFINES
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<ll, ll>
+#define pi pair<int, int>
+#define mp make_pair
+#define setbits(x) __builtin_popcountll(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define endl "\n"
+#define debug(x) cout << #x << " -> " << x << "\n";
+const ll mod = 1000000007;
+const ll MOD = 998244353;
+const ll inf = 1e18;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+	c_p_c();
+
+	ll t;
+	cin >> t;
+	while (t--)
+	{
+		ll n;
+		cin >> n;
+		ll arr[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+
+		sort(arr, arr + n);
+		int ans = arr[0] + arr[1];
+		cout << ans << endl;
+
+	}
+
+	return 0;
+}
