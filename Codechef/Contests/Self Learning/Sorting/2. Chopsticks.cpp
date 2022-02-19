aa061e8bee5fc15a97d94bcefcbd185604722f9c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-19 16:31:03 +0530

    Create 2. Chopsticks.cpp

---
+++ Codechef/Contests/Self Learning/Sorting/2. Chopsticks.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+#define PI 3.14
+#define _USE_MATH_DEFINES
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<ll, ll>
+#define pi pair<int, int>
+#define mp make_pair
+#define setbits(x) __builtin_popcountll(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define endl "\n"
+#define debug(x) cout << #x << " -> " << x << "\n";
+const ll mod = 1000000007;
+const ll MOD = 998244353;
+const ll inf = 1e18;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+	c_p_c();
+
+	ll n, d;
+	cin >> n >> d;
+	ll arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	ll count = 0;
+	for (int i = 0; i < n - 1;)
+	{
+		if (arr[i + 1] - arr[i] <= d)
+		{
+			count++;
+			i += 2;
+		}
+		else
+		{
+			i += 1;
+		}
+	}
+	cout << count << endl;
+	return 0;
+}
