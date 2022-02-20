ef8f9cf1ac7e8cd659a9ce3124201909afcb0d15
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-20 10:22:59 +0530

    Update 2 .4 sum.cpp

--- Striver's SDE Sheet/Day 4/2 .4 sum.cpp
+++ Striver's SDE Sheet/Day 4/2 .4 sum.cpp
@@ -3,36 +3,36 @@ public:
     // Brute force
     // using set 
     // using 3 pointer + binary search(stL) 
-    vector<vector<int>> fourSum(vector<int>& nums, int target) 
-    {
-        int n =nums.size();
-        sort(nums.begin(),nums.end());
-        
-        set<vector<int>>sv;
+    vector<vector<int>> fourSum(vector<int>& nums, int target)
+	{
+		int n = nums.size();
+		sort(nums.begin(), nums.end());
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                for(int k=j+1;k<n;k++)
-                {
-                    int x = (long long)target - (long long)nums[i]-(long long)nums[j]-(long long)nums[k];
-                    if(binary_search(nums.begin()+k+1,nums.end(),x))
-                    {
-                        vector<int>v;
-                        v.push_back(nums[i]);
-                        v.push_back(nums[j]);
-                        v.push_back(nums[k]);
-                        v.push_back(x);
-                        sort(v.begin(),v.end());
-                        sv.insert(v);
-                    }
-                }
-            }
-        }
-        vector<vector<int>>res(sv.begin(),sv.end());
-        return res;
-    }
+		set<vector<int>>sv;
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				for (int k = j + 1; k < n; k++)
+				{
+					int x = (long long)target - (long long)nums[i] - (long long)nums[j] - (long long)nums[k];
+					if (binary_search(nums.begin() + k + 1, nums.end(), x))
+					{
+						vector<int>v;
+						v.push_back(nums[i]);
+						v.push_back(nums[j]);
+						v.push_back(nums[k]);
+						v.push_back(x);
+						sort(v.begin(), v.end());
+						sv.insert(v);
+					}
+				}
+			}
+		}
+		vector<vector<int>>res(sv.begin(), sv.end());
+		return res;
+	}
     
     
     // using two pointer appraoch 
