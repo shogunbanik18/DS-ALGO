02857f3cde31cb7d48907c4f3e9fe7edd746b271
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-21 19:09:09 +0530

    Create  MAKEDIV3.cpp

---
+++ Codechef/Contests/Self Learning/Basic Programming Concepts/MAKEDIV3.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+#define PI 3.14
+#define _USE_MATH_DEFINES
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<ll, ll>
+#define pi pair<int, int>
+#define mp make_pair
+#define setbits(x) __builtin_popcountll(x)
+#define sp(x, y) fixed << setprecision(y) << x
+#define endl "\n"
+#define debug(x) cout << #x << " -> " << x << "\n";
+const ll mod = 1000000007;
+const ll MOD = 998244353;
+const ll inf = 1e18;
+
+void c_p_c()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main()
+{
+	c_p_c();
+
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++)
+		{
+			if (i == 1 or i == n)
+			{
+				cout << '3';
+			}
+			else
+			{
+				cout << '0';
+			}
+		}
+		cout << endl;
+
+	}
+	return 0;
+}
