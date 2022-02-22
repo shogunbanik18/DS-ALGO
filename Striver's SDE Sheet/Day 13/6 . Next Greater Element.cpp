8702933ca0c17bcaf8fa13b31ae403b1fce105bc
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-23 01:17:36 +0530

    Create 6 . Next Greater Element.cpp

---
+++ Striver's SDE Sheet/Day 13/6 . Next Greater Element.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+vector<int> nextGreater(vector<int> &arr, int n) 
+{
+    stack <int> st;
+    vector<int>ans(n,0);
+    for(int i=n-1;i>=0;i--)
+    {
+        while(!st.empty() and arr[i]>=st.top())
+        {
+            st.pop();
+        }
+     	if(i<n)
+        {
+            if(!st.empty())
+            {
+                ans[i]=st.top();
+            }
+            else
+            {
+                ans[i]=-1;
+            }
+        }
+        st.push(arr[i]);
+    }
+	return ans;
+}
