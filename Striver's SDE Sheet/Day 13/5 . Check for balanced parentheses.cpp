e5acefbdf363782d4e8b671e8885e5eb08984f40
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-22 13:50:51 +0530

    Update 5 . Check for balanced parentheses.cpp

--- Striver's SDE Sheet/Day 13/5 . Check for balanced parentheses.cpp
+++ Striver's SDE Sheet/Day 13/5 . Check for balanced parentheses.cpp
@@ -60,3 +60,42 @@ public:
         return ans;
     }
 };
+
+// striver's approach 
+class Solution {
+public:
+    // tc:o(n)
+    // sc:o(n)
+    bool isValid(string s) 
+    {
+        stack<char>st;
+        int n=s.size();
+        bool ans=true;
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(' or s[i]=='{' or s[i] =='[')
+            {
+                st.push(s[i]);
+            }
+            
+            else 
+            {
+                if(st.empty())
+                {
+                    return false;
+                }
+                char c=st.top();
+                st.pop();
+                if((s[i]==')' and c=='(') or (s[i]=='}' and c=='{') or (s[i]==']' and c=='[') )
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
