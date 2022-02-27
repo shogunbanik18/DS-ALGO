fc2af9615866aead347157ae449f9486bb87e23d
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-02-27 12:57:08 +0530

    Update 2 .Matrix Median.cpp

--- Striver's SDE Sheet/Day 11/2 .Matrix Median.cpp
+++ Striver's SDE Sheet/Day 11/2 .Matrix Median.cpp
@@ -72,3 +72,55 @@ public:
         // code here          
     }
 };
+
+    
+    
+//     Self Dry Run 
+    class Solution{   
+public:
+    
+    int findsmallerelements(vector<int> &row,int mid)
+    {
+        int l=0;
+        int h=row.size()-1;
+        while(l<=h)
+        {
+            int md = (l+h)/2;
+            if(row[md]<=mid)
+            {
+                l=md+1;
+            }
+            else
+            {
+                h=md-1;
+            }
+        }
+        return l;
+    }
+
+    int median(vector<vector<int>> &matrix, int r, int c)
+    {
+      int low=1;
+      int high=1e9;
+      while(low<=high)
+      {
+          int mid = (low +high)/2;
+          int count=0;
+          for(int i=0;i<r;i++)
+          {
+              count+= findsmallerelements(matrix[i],mid);
+          }
+           
+          if(count<=(r*c)/2)
+          {
+              low=mid+1;
+          }
+          else
+          {
+              high=mid-1;
+          }
+      }
+      return low;
+    }
+
+};
