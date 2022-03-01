c969622eefff52bf70bbe8f75e1c2bc07f32703d
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-01 13:40:55 +0530

    Update 10. Celebrity Problem.cpp

--- Striver's SDE Sheet/Day 14/10. Celebrity Problem.cpp
+++ Striver's SDE Sheet/Day 14/10. Celebrity Problem.cpp
@@ -38,22 +38,64 @@ class Solution
     // sc:o(1)
     int celebrity(vector<vector<int> >& M, int n) 
     {
-       int c=0;
-       for(int i=1;i<n;i++)
-       {
-           if(M[c][i]==1)
-           {
-               c=i;
-           }
-       }
+      int c=0;
+      for(int i=1;i<n;i++)
+      {
+          if(M[c][i]==1)
+          {
+              c=i;
+          }
+      }
        
-       for(int i=0;i<n;i++)
-       {
-           if(i!=c and (M[c][i]==1 or M[i][c]==0))
-           {
-               return -1;
-           }
-       }
-       return c;
+      for(int i=0;i<n;i++)
+      {
+          if(i!=c and (M[c][i]==1 or M[i][c]==0))
+          {
+              return -1;
+          }
+      }
+      return c;
+    }
+    
+    // using stack 
+    // tc: o(n)
+    // sc:o(n)
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int>st;
+        for(int i=0;i<n;i++)
+        {
+            st.push(i);
+        }
+        
+        while(st.size()!=1)
+        {
+            int a=st.top();
+            st.pop();
+            int b=st.top();
+            st.pop();
+            
+            if(M[a][b]==1)
+            {
+                st.push(b);
+            }
+            else
+            {
+                st.push(a);
+            }
+        }
+        
+        int ans=st.top();
+        for(int i=0;i<n;i++)
+        {
+            if(i!=ans)
+            {
+                if(M[ans][i]==1 or M[i][ans]==0)
+                {
+                    return -1;
+                }
+            }
+        }
+        return ans;
     }
 };
