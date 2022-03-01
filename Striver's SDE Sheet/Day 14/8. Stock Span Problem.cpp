f341ee23b1834ddc8f08b4212b3a789eb5e48492
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-01 12:49:21 +0530

    Update 8. Stock Span Problem.cpp

--- Striver's SDE Sheet/Day 14/8. Stock Span Problem.cpp
+++ Striver's SDE Sheet/Day 14/8. Stock Span Problem.cpp
@@ -1,3 +1,34 @@
+// Leetcode 
+class StockSpanner 
+{
+    private: 
+         stack<pair<int,int>>st;
+    public:
+    StockSpanner() 
+    {
+        
+    }
+    int next(int price) 
+    {
+        int ans=1;
+        while(!st.empty() and st.top().first<=price)
+        {
+            ans+=st.top().second;
+            st.pop();
+        }        
+        st.push({price,ans});
+        return ans;
+    }
+};
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+
+
 // Code Implementation from scratch 
 #include <bits/stdc++.h>
 #include <math.h>
