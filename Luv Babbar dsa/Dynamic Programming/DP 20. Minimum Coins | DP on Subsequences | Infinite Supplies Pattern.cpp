5d33f72aea718c6666d3881c530065f6ba667513
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-03 20:02:54 +0530

    Update DP 20. Minimum Coins | DP on Subsequences | Infinite Supplies Pattern.cpp

--- Luv Babbar dsa/Dynamic Programming/DP 20. Minimum Coins | DP on Subsequences | Infinite Supplies Pattern.cpp
+++ Luv Babbar dsa/Dynamic Programming/DP 20. Minimum Coins | DP on Subsequences | Infinite Supplies Pattern.cpp
@@ -102,7 +102,7 @@ int minimumElements(vector<int> &nums, int target)
                 take = 1+dp[ind][T-nums[ind]];
             }
             dp[ind][T]= min(take,nottake);
-        }
+        }a
     }
     
    	int ans= dp[n-1][target];
@@ -155,3 +155,174 @@ int minimumElements(vector<int> &nums, int target)
     }
     return ans;
 }
+
+
+// Leetcode
+class Solution {
+public:
+    // Greedy does not works due to uniformity
+    // eg [9,6,5,1] min coins can be 2 but our ans gives 3
+    
+//     // using recursion 
+//     // tc: exponential 
+//     // sc: auxiliary stack space
+//     int f(int ind,int target,vector<int>&arr)
+//     {
+//         if(ind==0)
+//         {
+//             if(target%arr[0]==0)
+//             {
+//                 return target/arr[0];
+//             }
+//             else
+//             {
+//                 return 1e9;
+//             }
+//         }
+//         int nottake = 0 +f(ind-1,target,arr);
+//         int take=1e9;
+//         if(arr[ind]<=target)
+//         {
+//             take = 1+ f(ind,target-arr[ind],arr);
+//         }
+//         return min(take,nottake);
+//     }
+    
+//     int coinChange(vector<int>& coins, int amount) 
+//     {
+//         int n=coins.size();
+//         int ans= f(n-1,amount,coins);
+//         if(ans>=1e9)
+//         {
+//             return -1;
+//         }
+//         return ans;
+//     }
+    
+    
+    // using Memoisation
+   // tc: o(n*target)
+    // sc: o(n*target) + o(n) auxiliary stack space
+//     int f(int ind,int target,vector<int>&arr,vector<vector<int>>&dp)
+//     {
+//         if(ind==0)
+//         {
+//             if(target%arr[0]==0)
+//             {
+//                 return target/arr[0];
+//             }
+//             else
+//             {
+//                 return 1e9;
+//             }
+//         }
+//         if(dp[ind][target]!=-1)
+//         {
+//             return dp[ind][target];
+//         }
+//         int nottake = 0 +f(ind-1,target,arr,dp);
+//         int take=1e9;
+//         if(arr[ind]<=target)
+//         {
+//             take = 1+ f(ind,target-arr[ind],arr,dp);
+//         }
+//         return dp[ind][target]= min(take,nottake);
+//     }
+    
+//     int coinChange(vector<int>& coins, int amount) 
+//     {
+//         int n=coins.size();
+//         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+//         int ans= f(n-1,amount,coins,dp);
+//         if(ans>=1e9)
+//         {
+//             return -1;
+//         }
+//         return ans;
+//     }
+    
+//     // using Tabulation
+//     // tc: o(n*target)
+//     // sc: o(n*target)
+//     int coinChange(vector<int>& coins, int amount) 
+//     {
+//         int n=coins.size();
+//         vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        
+//         for(int i=0;i<=amount;i++)
+//         {
+//             if(i%coins[0]==0)
+//             {
+//                 dp[0][i]=i/coins[0];
+//             }
+//             else
+//             {
+//                 dp[0][i]=1e9;
+//             }
+//         }
+        
+//         for(int ind=1;ind<n;ind++)
+//         {
+//             for(int target=0;target<=amount;target++)
+//             {
+//                 int nottake = 0 +dp[ind-1][target];
+//                 int take=1e9;
+//                 if(coins[ind]<=target)
+//                 {
+//                     take = 1+ dp[ind][target-coins[ind]];
+//                 }
+//                 dp[ind][target]= min(take,nottake);
+//             }
+//         }
+        
+//         int ans= dp[n-1][amount];
+//         if(ans>=1e9)
+//         {
+//             return -1;
+//         }
+//         return ans;
+//     }
+    
+    // using Tabulation space optimisation
+    // tc: o(n*target)
+    // sc: o(n)
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int n=coins.size();
+        vector<int>prev(amount+1,0),curr(amount+1,0);
+        
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+            {
+                prev[i]=i/coins[0];
+            }
+            else
+            {
+                prev[i]=1e9;
+            }
+        }
+        
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int target=0;target<=amount;target++)
+            {
+                int nottake = 0 + prev[target];
+                int take=1e9;
+                if(coins[ind]<=target)
+                {
+                    take = 1+ curr[target-coins[ind]];
+                }
+                curr[target]= min(take,nottake);
+            }
+            prev=curr;
+        }
+        
+        int ans= prev[amount];
+        if(ans>=1e9)
+        {
+            return -1;
+        }
+        return ans;
+    }
+};
