fe02911306c426d21d76c4044447d4dabba9f26c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-05 21:09:10 +0530

    Update 2 .Minimum number of platforms required for a railway.cpp

--- Striver's SDE Sheet/Day 8/2 .Minimum number of platforms required for a railway.cpp
+++ Striver's SDE Sheet/Day 8/2 .Minimum number of platforms required for a railway.cpp
@@ -36,3 +36,53 @@ class Solution{
         return platform;
     }
 };
+
+
+// coding ninja 
+// tc: o(nlogn) 
+// sc:o(1)
+int calculateMinPatforms(int at[], int dt[], int n) 
+{
+    sort(at,at+n);
+    sort(dt,dt+n);
+    int i=1;
+    int j=0;
+    int count=1,ans=1;
+    while(i<n and j<n)
+    {
+        if(at[i]<=dt[j])
+        {
+            count++;
+            i++;
+        }
+        else
+        {
+            count--;
+            j++;
+        }
+        ans=max(ans,count);
+    }
+    return ans;
+    // Write your code here.
+}
+
+// brute force
+// tc: o(n*n)
+// sc:o(1)
+int calculateMinPatforms(int at[], int dt[], int n) 
+{
+    int ans=1;
+    for(int i=0;i<n;i++)
+    {
+        int count=1;
+        for(int j=i+1;j<n;i++)
+        {
+            if((at[i]>=at[j] and at[i]<=dt[j]) or (at[i]<=at[j] and at[j]<=dt[i]))
+            {
+                count++;
+            }
+        }
+        ans=max(ans,count);
+    }
+    // Write your code here.
+}
