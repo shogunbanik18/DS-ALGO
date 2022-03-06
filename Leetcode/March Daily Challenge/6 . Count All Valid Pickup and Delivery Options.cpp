2f06b8a29bd0b32015a45f3face20924dd7890c8
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-06 21:21:31 +0530

    Create 6 . Count All Valid Pickup and Delivery Options.cpp

---
+++ Leetcode/March Daily Challenge/6 . Count All Valid Pickup and Delivery Options.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // using maths permutation and Recursion Combinatorics
+    int fact(int n)
+    {
+        if(n==0 or n==1)
+        {
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    
+    int countOrders(int n) 
+    {
+        // int mod =1000000007;
+        // int pickup=fact(n);
+        // int delivery=1;
+        // for(int i=1;i<=2*n-1;i+=2)
+        // {
+        //     delivery =delivery*i;
+        // }
+        // // cout<<pickup<<" " << delivery<<endl;
+        // int count=(pickup*delivery);
+        // return count%mod;
+        
+        long res=1,mod=1000000007;
+        for(int i=1;i<=n;i++)
+        {
+            res=res*i;
+            res=res%mod;
+            res=res*(2*i-1);
+            res=res%mod;
+        }
+        return res%mod;
+    }
+};
