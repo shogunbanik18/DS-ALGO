1f7d470abbe86acb858f066c0b64214192bc39f5
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-06 21:22:49 +0530

    Update 6 . Count All Valid Pickup and Delivery Options.cpp

--- Leetcode/March Daily Challenge/6 . Count All Valid Pickup and Delivery Options.cpp
+++ Leetcode/March Daily Challenge/6 . Count All Valid Pickup and Delivery Options.cpp
@@ -1,28 +1,10 @@
 class Solution {
 public:
     // using maths permutation and Recursion Combinatorics
-    int fact(int n)
-    {
-        if(n==0 or n==1)
-        {
-            return 1;
-        }
-        return n*fact(n-1);
-    }
-    
+//     tc :o(n) +o(n)
+//     sc:o(1)
     int countOrders(int n) 
     {
-        // int mod =1000000007;
-        // int pickup=fact(n);
-        // int delivery=1;
-        // for(int i=1;i<=2*n-1;i+=2)
-        // {
-        //     delivery =delivery*i;
-        // }
-        // // cout<<pickup<<" " << delivery<<endl;
-        // int count=(pickup*delivery);
-        // return count%mod;
-        
         long res=1,mod=1000000007;
         for(int i=1;i<=n;i++)
         {
@@ -33,4 +15,39 @@ public:
         }
         return res%mod;
     }
+    
+    
+//     Overflow 
+//     int fact(int n)
+//     {
+//         if(n==0 or n==1)
+//         {
+//             return 1;
+//         }
+//         return n*fact(n-1);
+//     }
+    
+//     int countOrders(int n) 
+//     {
+//         // int mod =1000000007;
+//         // int pickup=fact(n);
+//         // int delivery=1;
+//         // for(int i=1;i<=2*n-1;i+=2)
+//         // {
+//         //     delivery =delivery*i;
+//         // }
+//         // // cout<<pickup<<" " << delivery<<endl;
+//         // int count=(pickup*delivery);
+//         // return count%mod;
+        
+//         long res=1,mod=1000000007;
+//         for(int i=1;i<=n;i++)
+//         {
+//             res=res*i;
+//             res=res%mod;
+//             res=res*(2*i-1);
+//             res=res%mod;
+//         }
+//         return res%mod;
+//     }
 };
