5effbf1639686bdbf96268d6a2c612c4acd0aec3
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-08 01:09:32 +0530

    Update DP 29. Minimum Insertions to Make String Palindrome.cpp

--- Luv Babbar dsa/Dynamic Programming/DP 29. Minimum Insertions to Make String Palindrome.cpp
+++ Luv Babbar dsa/Dynamic Programming/DP 29. Minimum Insertions to Make String Palindrome.cpp
@@ -3,6 +3,39 @@ public:
     // intutition 
     // find the longest palindromic subsequence length and then n-length
     
+    // using memoisation (TLE) 
+    // tc: o(n*m)
+    // sc:o(n*m)+ o(n+m)
+//     int lcs(int ind1,int ind2,string s1,string s2,vector<vector<int>>&dp)
+//     {
+//         if(ind1==0 or ind2==0)
+//         {
+//             return 0;
+//         }
+        
+//         if(dp[ind1][ind2]!=-1)
+//         {
+//             return dp[ind1][ind2];
+//         }
+        
+//         if(s1[ind1-1]==s2[ind2-1])
+//         {
+//             return dp[ind1][ind2]= 1 + lcs(ind1-1,ind2-1,s1,s2,dp);
+//         }
+//         else
+//         {
+//             return dp[ind1][ind2]=0 + max(lcs(ind1-1,ind2,s1,s2,dp),lcs(ind1,ind2-1,s1,s2,dp));
+//         }
+//     }
+    
+//      int palindromic_lcs(string s1,string s2)
+//      {
+//          int n=s1.size();
+//          int m=s2.size();
+//          vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+//          return lcs(n,m,s1,s2,dp);
+//      }
+    
     // using tabulation dp 
     // tc: o(n*m)
     // sc:o(n+m)
@@ -46,7 +79,7 @@ public:
         string a=s;
         reverse(s.begin(),s.end());
         int ans=palindromic_lcs(a,s);
-        ans =n-ans;
+        ans=n- ans;
         return ans;
     }
 };
