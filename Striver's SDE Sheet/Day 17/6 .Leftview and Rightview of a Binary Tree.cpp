a2adc3dab0e09befbcc521c7a14e0c394c1ee582
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-10 01:52:06 +0530

    Update 6 .Leftview and Rightview of a Binary Tree.cpp

--- Striver's SDE Sheet/Day 17/6 .Leftview and Rightview of a Binary Tree.cpp
+++ Striver's SDE Sheet/Day 17/6 .Leftview and Rightview of a Binary Tree.cpp
@@ -67,3 +67,44 @@ vector<int> rightView(Node *root)
     f(root,0,v);
     return v;
 }
+
+
+// Leetcode 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// Right View 
+class Solution {
+public:
+    // using recursion 
+    // tc:o(n)
+    // sc:o(H)
+    void f(TreeNode*node,int level,vector<int>&v)
+    {
+        if(node==NULL)
+        {
+            return;
+        }
+        if(v.size()==level)
+        {
+            v.push_back(node->val);
+        }
+        f(node->right,level+1,v);
+        f(node->left,level+1,v);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        vector<int>v;
+        f(root,0,v);
+        return v;
+    }
+};
