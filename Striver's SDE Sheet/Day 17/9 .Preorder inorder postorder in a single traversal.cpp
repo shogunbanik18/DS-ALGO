7059182b51a2765ec0feea8181e1e7e2a133273e
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-10 15:52:41 +0530

    Create 9 .Preorder inorder postorder in a single traversal.cpp

---
+++ Striver's SDE Sheet/Day 17/9 .Preorder inorder postorder in a single traversal.cpp
@@ -0,0 +1,79 @@
+/************************************************************
+
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+// Coding Ninjas
+// tc:o(n)+o(n)+o(n)
+// sc: o(n)+o(n)+o(n)
+#include<bits/stdc++.h>
+
+vector<vector<int>> getTreeTraversal(BinaryTreeNode<int> *root)
+{
+    vector<vector<int>>ds(3);
+    
+    if(root==NULL)
+    {
+        return ds;
+    }
+    
+    stack<pair< BinaryTreeNode<int> *,int>>st;
+    st.push({root,1});
+    
+    while(!st.empty())
+    {
+        auto a=st.top();
+        st.pop();
+        
+//         BinaryTreeNode<int> *node=a.first;
+//         int num=a.second;
+//         answer will be inorder ,preorder ,postorder
+        
+//      Preorder
+        if(a.second==1)
+        {
+            ds[0].push_back(a.first->data);
+            a.second++;
+            st.push(a);
+            
+            if(a.first->left!=NULL)
+            {
+                st.push({a.first->left,1});
+            }
+        }
+        
+//        Inorder
+        else if(a.second==2)
+        {
+            ds[1].push_back(a.first->data);
+            a.second++;
+            st.push(a);
+            
+            if(a.first->right!=NULL)
+            {
+                st.push({a.first->right,1});
+            }
+        }
+        
+//      Postorder
+        else
+        {
+            ds[2].push_back(a.first->data);
+        }
+    }
+    return ds;
+}
