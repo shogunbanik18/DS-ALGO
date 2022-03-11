964fed9e68989ef5c6fbc9d55402ce356c12226e
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-11 14:02:08 +0530

    Update 1 . Binary Tree Level Order Traversal.cpp

--- Striver's SDE Sheet/Day 18/1 . Binary Tree Level Order Traversal.cpp
+++ Striver's SDE Sheet/Day 18/1 . Binary Tree Level Order Traversal.cpp
@@ -40,41 +40,41 @@ public:
     // using bfs 
     // level order traversal using queue 
     // using itertion 
-//     vector<vector<int>> levelOrder(TreeNode* root) 
-//     {
-//         vector<vector<int>>ds;
-//         if(root==NULL)
-//         {
-//             return ds;
-//         }
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>>ds;
+        if(root==NULL)
+        {
+            return ds;
+        }
         
-//         queue<TreeNode*>q;
-//         q.push(root);
+        queue<TreeNode*>q;
+        q.push(root);
         
-//         while(!q.empty())
-//         {
-//             vector<int>v;
-//             int size=q.size();
+        while(!q.empty())
+        {
+            vector<int>v;
+            int size=q.size();
             
-//             for(int i=0;i<size;i++)
-//             {
-//                 TreeNode* node =q.front();
-//                 q.pop();
-//                 v.push_back(node->val);
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* node =q.front();
+                q.pop();
+                v.push_back(node->val);
                 
-//                 if(node->left!=NULL)
-//                 {
-//                     q.push(node->left);
-//                 }
+                if(node->left!=NULL)
+                {
+                    q.push(node->left);
+                }
 
-//                 if(node->right!=NULL)
-//                 {
-//                     q.push(node->right);
-//                 }
+                if(node->right!=NULL)
+                {
+                    q.push(node->right);
+                }
                 
-//             }
-//             ds.push_back(v);
-//         }
-//         return ds;
-//     }
+            }
+            ds.push_back(v);
+        }
+        return ds;
+    }
 };
