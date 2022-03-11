037fb89ee68aa5638878ab08744afe11813791da
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-11 11:28:25 +0530

    Update 1 .Rotate a LinkedList.cpp

--- Striver's SDE Sheet/Day 7/1 .Rotate a LinkedList.cpp
+++ Striver's SDE Sheet/Day 7/1 .Rotate a LinkedList.cpp
@@ -39,28 +39,51 @@ public:
     // optimal approach 
     // tc:o(n)+o(n-(n%k))
     // sc:o(1)
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    
+    // tc:o(n)+o(n-(n%k))
+    // sc:o(1)
+    
     ListNode* rotateRight(ListNode* head, int k) 
     {
         if(head==NULL or head->next==NULL or k==0)
         {
             return head;
         }
+        
         ListNode*curr=head;
         int count=1;
+        
         while(curr->next!=NULL)
         {
             count++;
             curr=curr->next;
+            // cout<<curr->val<<" ";
         }
         curr->next=head;
+        
         k=k%count;
-        k=count-k;
+        k= count-k;
+        // cout<<count<<" "<<k<<endl;
+        
         while(k--)
         {
             curr=curr->next;
         }
-        head=curr->next;
+        ListNode*ans=curr->next;
         curr->next=NULL;
-        return head;
+        return ans; 
     }
+};
 };
