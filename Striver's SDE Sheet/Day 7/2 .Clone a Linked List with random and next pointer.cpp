c62fafa763e82864e4116f6e8ce14c3c612e756c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-12 12:24:12 +0530

    Update 2 .Clone a Linked List with random and next pointer.cpp

--- Striver's SDE Sheet/Day 7/2 .Clone a Linked List with random and next pointer.cpp
+++ Striver's SDE Sheet/Day 7/2 .Clone a Linked List with random and next pointer.cpp
@@ -16,39 +16,91 @@ public:
 
 class Solution {
 public:
-    // using Hashmap Brute Force
-    // tc:o(n)+o(n)
-    // sc:o(n)
-    Node* copyRandomList(Node* head)
+    Node* copyRandomList(Node* head) 
     {
-        unordered_map<Node*,Node*>Hashmap;
         Node*curr=head;
         while(curr!=NULL)
         {
-            Node*a= new Node(curr->val);
-            Hashmap[curr]=a;
+            Node*p= new Node(curr->val);
+            p->next=curr->next;
+            curr->next=p;
+            curr=curr->next->next;
+        }
+        
+        Node* temp=head;
+        while(temp!=NULL)
+        {
+            if(temp->random!=NULL)
+            {
+                temp->next->random= temp->random->next;
+            }
+            
+            temp=temp->next->next;
+        }
+        
+        Node*dummy = new Node(0);
+        Node*a =dummy;
+        Node*fast;
+        Node*it=head;
+        
+        while(it!=NULL)
+        {
+            fast=it->next->next;
+            a->next=it->next;
+            it->next=fast;
+            a=a->next;
+            it=fast;
+        }
+        
+        Node* newHead=dummy->next;
+        return newHead;
+    }
+};
+
+using map 
+Brute force 
+   using Hashmap Brute Force
+    tc:o(n)+o(n)
+    sc:o(n)
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
+    {
+        map<Node*,Node*>mp;
+        Node*curr=head;
+        while(curr!=NULL)
+        {
+            Node*a=new Node(curr->val);
+            mp[curr]=a;
             curr=curr->next;
         }
+        
         Node*temp=head;
         while(temp!=NULL)
         {
-            Node*p=Hashmap[temp];
-            p->next =(temp->next!=NULL)?Hashmap[temp->next]:NULL;
-            p->random =(temp->random!=NULL)?Hashmap[temp->random]:NULL;
+            Node* p = mp[temp];
+            p->next =(temp->next!=NULL)?mp[temp->next]:NULL;
+            p->random = (temp->random!=NULL)?mp[temp->random]:NULL;
             temp=temp->next;
         }
-        return Hashmap[head];
+        return mp[head];
     }
-    
-    // Most optimised solution 
-    // tc:o(n)+o(n)+o(n)
-    // sc:o(1)
-    Node* copyRandomList(Node* head)
+};
+
+
+// Same as 1
+// using concept of deep copy 
+// Most optimised solution 
+//     tc:o(n)+o(n)+o(n)
+//     sc:o(1)
+class Solution {
+public:
+    Node* copyRandomList(Node* head) 
     {
         Node*curr=head;
         while(curr!=NULL)
         {
-            Node*p=new Node(curr->val);
+            Node*p =new Node(curr->val);
             p->next=curr->next;
             curr->next=p;
             curr=curr->next->next;
@@ -57,19 +109,19 @@ public:
         Node*temp=head;
         while(temp!=NULL)
         {
-            // edge case 
             if(temp->random!=NULL)
             {
-                temp->next->random=temp->random->next;
+                temp->next->random = temp->random->next;
             }
-            temp=temp->next->next;
+            temp = temp->next->next;
         }
         
-        // making the links
-        Node*dummy=new Node(0);
-        Node*iter=head;
-        Node*a=dummy;
-        Node*fast;
+        // Making links
+        Node*dummy= new Node(0);
+        Node* iter=head;
+        Node* a=dummy;
+        Node* fast;
+        
         while(iter!=NULL)
         {
             fast=iter->next->next;
@@ -79,5 +131,5 @@ public:
             iter=fast;
         }
         return dummy->next;
-    }  
+    }
 };
