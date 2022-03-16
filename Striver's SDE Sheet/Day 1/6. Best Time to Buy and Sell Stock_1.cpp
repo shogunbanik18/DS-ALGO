96a750b28531f1c716df3638fa992ac4fef08716
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-17 01:55:18 +0530

    Update 6. Best Time to Buy and Sell Stock_1.cpp

--- Striver's SDE Sheet/Day 1/6. Best Time to Buy and Sell Stock_1.cpp
+++ Striver's SDE Sheet/Day 1/6. Best Time to Buy and Sell Stock_1.cpp
@@ -1,53 +1,56 @@
+// tc: o(n^2)
+// sc: o(1)
 class Solution {
 public:
-    // Brute force 
-    // tc:O(N^2)
-    // SC: O(1)
-    int maxProfit(vector<int>& prices) 
+    int maxProfit(vector<int>& prices)
     {
+        int ans=0;
         int n=prices.size();
-        int maxpro=0;
         for(int i=0;i<n-1;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                if(prices[j]>prices[i])
-                {
-                    maxpro=max(maxpro,prices[j]-prices[i]);
-                }
+                    ans = max(ans, prices[j] - prices[i]);
             }
         }
-        return maxpro;
+        return ans;
     }
-    
-    // tc : O(N)
-    // sc:  O(1)
-    int maxProfit(vector<int>& prices) 
-    {
-        int n=prices.size();
-        int maxprofit=0;
-        int minsofar=prices[0];
-        for(int i=0;i<n;i++)
-        {
-            minsofar=min(minsofar,prices[i]);
-            int profit=prices[i]-minsofar;
-            maxprofit= max(maxprofit,profit);
-        }
-        return maxprofit;
-    } 
 };
 
-  // tc : O(N)
-    // sc:  O(1)
-//     int maxProfit(vector<int>& prices) 
-//     {
-//         int n=prices.size();
-//         int mini=INT_MAX;
-//         int maxi=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             mini=min(mini,prices[i]);
-//             maxi=max(maxi,prices[i]-mini);
-//         }
-//         return maxi;
-//     }
+// tc: o(n)
+// sc: o(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int maxprofit=0;
+        int minsofar = prices[0];
+        int n=prices.size();
+        for(int i=0;i<n-1;i++)
+        {
+            minsofar =min(prices[i],minsofar);
+            int profit = max( profit,prices[i] - minsofar);
+            maxprofit  = max (maxprofit,profit);
+        }
+        return maxprofit;
+    }
+};
+
+// using dp based approach 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int profit=0;
+        int mini= prices[0];
+        int n=prices.size();
+        
+        for(int i=1;i<n;i++)
+        {
+            int cost = prices[i]-mini;
+            profit = max(profit,cost);
+            mini = min (mini,prices[i]);
+        }
+        return profit;
+    }
+};
