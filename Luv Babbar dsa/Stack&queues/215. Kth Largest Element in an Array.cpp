8f0f791c3347e9309fc159c64e5615a2b9ed433b
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-20 15:48:33 +0530

    Create 215. Kth Largest Element in an Array.cpp

---
+++ Luv Babbar dsa/Stack&queues/215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,39 @@
+// using sorting and searching 
+// tc :o(nlogn)
+// sc :o (1)
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k)
+//     {
+//         sort(nums.begin(),nums.end());
+        
+//         int x = nums.size()-k;
+//         return nums[x];
+//     }
+// };
+
+// using priority queue 
+// tc : o(nlogk)
+// sc : o(1)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        priority_queue<int>pq;
+        for(auto x :nums )
+        {
+            pq.push(x);
+        }
+        
+        // cout<<pq.top()<<endl;
+        
+        int x = k-1;
+        while(x--)
+        {
+            pq.pop();
+        }
+        
+        int a = pq.top();
+        return a;
+    }
+};
