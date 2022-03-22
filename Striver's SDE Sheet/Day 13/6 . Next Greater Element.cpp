25d9a4de851daba85ee4fd4bd409d9c0f750d766
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-23 02:34:34 +0530

    Update 6 . Next Greater Element.cpp

--- Striver's SDE Sheet/Day 13/6 . Next Greater Element.cpp
+++ Striver's SDE Sheet/Day 13/6 . Next Greater Element.cpp
@@ -66,3 +66,38 @@ public:
         return ans;
     }
 };
+
+
+// nge 1  gfg 
+class Solution
+{
+    public:
+    //Function to find the next greater element for each element of the array.
+    vector<long long> nextLargerElement(vector<long long> arr, int n)
+    {
+        stack<long long>st;
+        vector<long long> v(n,0);
+        
+        // traversing from the back 
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() and st.top()<=arr[i])
+            {
+                st.pop();
+            }
+            
+            if(st.empty())
+            {
+                v[i] = -1;
+            }
+            else
+            {
+                v[i] = st.top();
+            }
+            
+            st.push(arr[i]);
+        }
+        return v;
+    
+    }
+};
