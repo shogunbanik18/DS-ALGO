6d3863dbc7b121ba4ccb08f2572ddd695704bd93
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-23 18:13:29 +0530

    Update 5. Palindrome Partitioning.cpp

--- Striver's SDE Sheet/Day 9/5. Palindrome Partitioning.cpp
+++ Striver's SDE Sheet/Day 9/5. Palindrome Partitioning.cpp
@@ -44,3 +44,53 @@ public:
 };
 
 
+// leetcode 
+// using Recursion 
+class Solution 
+{
+    
+private:
+    
+    bool check(int a,int b,string s)
+    {
+        while(a<b)
+        {
+            if(s[a]!=s[b])
+            {
+                return false;
+            }
+            a++;
+            b--;
+        }
+        
+        return true;
+    }
+    
+    void f(int ind, string s,vector<vector<string>>&ans,vector<string>v)
+    {
+        int n = s.size();
+        if(ind == n)
+        {
+            ans.push_back(v);
+        }
+        
+        for(int i=ind;i<n;i++)
+        {
+            if(check(ind,i,s))
+            {
+                v.push_back(s.substr(ind , i- ind + 1 ));
+                f(i+1,s,ans,v);
+                v.pop_back();
+            }
+        }
+    }
+       
+public:
+    vector<vector<string>> partition(string s)
+    {
+        vector<vector<string>>ans;
+        vector<string>v;
+        f(0,s,ans,v);
+        return ans;
+    }
+};
