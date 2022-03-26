0e56d26e82cf7ff5e30039220d334e4cff01e1c1
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-26 18:03:04 +0530

    Update 6 . Lowest Common Ancestor of a Binary Search Tree.cpp

--- Striver's SDE Sheet/Day 20/6 . Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Striver's SDE Sheet/Day 20/6 . Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -8,17 +8,17 @@
  * };
  */
 
-// using recursion and travsersal 
+// Recursive Approach same as LCA of Binary Tree 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
         if(root==NULL or root==p or root==q)
         {
             return root;
         }
         
-        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* left  = lowestCommonAncestor(root->left,p,q);
         TreeNode* right = lowestCommonAncestor(root->right,p,q);
         
         if(left==NULL)
@@ -35,5 +35,58 @@ public:
         {
             return root;
         }
+    }
+};
+
+
+// Recursive Approach using the roperty of binary search 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if(root->val > p->val and root->val > q->val )
+        {
+            return lowestCommonAncestor(root->left , p,q);
+        }
+        
+        else if(root->val < p->val and root->val < q->val )
+        {
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        
+        else
+        {
+            return root;
+        }
+    }
+};
+
+
+// using binary search concept + iteration 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        TreeNode* curr = root;
+        
+        while(true)
+        {
+            if(curr->val > p->val and curr->val > q->val )
+            {
+                curr=curr->left;
+            }
+
+            else if(curr->val < p->val and curr->val < q->val )
+            {
+                curr=curr->right;
+            }
+            
+            else
+            {
+                break;
+            }
+        }
+        
+        return curr;
     }
 };
