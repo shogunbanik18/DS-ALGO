1f2f0697c728ceb9e6ffe58db0487e415a6c2f75
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-03-29 00:10:36 +0530

    Create 4.Check for Anagrams.cpp

---
+++ Striver's SDE Sheet/Day 16/4.Check for Anagrams.cpp
@@ -0,0 +1,46 @@
+// // Using Hashing
+// // tc : o(s) + o(t)
+// // sc : o(n) or(nlogn)
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        map<char,int>mp;
+        
+        // check for same size 
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        
+        // if same size then 
+        for(int i=0;i<s.size();i++)
+        {
+            mp[ s[i] ]++;
+            mp[ t[i] ] --; 
+        }
+        
+        for(auto x : mp)
+        {
+           if(x.second!=0)
+           {
+               return false;
+           }
+        }
+        
+        return true;
+    }
+};
+
+// using simple sorting 
+// tc : o(nlogn) + o(mlogm)
+// sc : o(1)
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
