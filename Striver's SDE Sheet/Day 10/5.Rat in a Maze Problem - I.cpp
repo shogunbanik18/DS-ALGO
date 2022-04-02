153b374e8ccb222be9c3fbefd39698253f14a68f
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-02 12:49:35 +0530

    Update 5.Rat in a Maze Problem - I.cpp

--- Striver's SDE Sheet/Day 10/5.Rat in a Maze Problem - I.cpp
+++ Striver's SDE Sheet/Day 10/5.Rat in a Maze Problem - I.cpp
@@ -1,102 +1,138 @@
-class Solution{
-    public:
-    // tc : o(4 ^(m*n))
-    // sc: O(m*n) ,Maximum Depth of the recursion tree(auxiliary space).
-    void solve(int i,int j,vector<vector<int>> &a,int n,vector<string>&ans,string move,vector<vector<int>>&vis)
+// use of a visited array 
+// tc : O(4^(m*n))
+// sc : O(m*n) stack space 
+class Solution
+{
+public:
+    
+    void f(int i ,int j,vector<vector<int>> &grid ,int n  ,vector<string> &ans,string ds ,vector<vector<int>> &vis)
     {
+        // base case 
         if(i==n-1 and j==n-1)
         {
-            ans.push_back(move);
+            ans.push_back(ds);
             return;
         }
         
-        // downwards direction 
-        if(i+1<n and !vis[i+1][j] and a[i+1][j]==1)
+        // Explore all the possible paths
+        // Downward
+        if(i+1<n and vis[i+1][j]==0 and grid[i+1][j]==1)
         {
             vis[i][j]=1;
-            solve(i+1,j,a,n,ans,move+'D',vis);
+            
+            ds.push_back('D');
+            f(i+1,j,grid,n,ans,ds,vis);
+            ds.pop_back();
+            
             vis[i][j]=0;
         }
         
-        // leftward direction 
-        if(j-1>=0 and !vis[i][j-1] and a[i][j-1]==1)
+        // Upward
+        if(i-1>=0 and vis[i-1][j]==0 and grid[i-1][j]==1)
         {
             vis[i][j]=1;
-            solve(i,j-1,a,n,ans,move+'L',vis);
+            
+            ds.push_back('U');
+            f(i-1,j,grid,n,ans,ds,vis);
+            ds.pop_back();
+            
             vis[i][j]=0;
         }
         
-        // Rightward direction 
-        if(j+1<n and  !vis[i][j+1] and a[i][j+1]==1)
+        // Rightward
+        if(j+1<n and vis[i][j+1]==0 and grid[i][j+1]==1)
         {
             vis[i][j]=1;
-            solve(i,j+1,a,n,ans,move+'R',vis);
+            
+            ds.push_back('R');
+            f(i,j+1,grid,n,ans,ds,vis);
+            ds.pop_back();
+            
             vis[i][j]=0;
         }
         
-        // upward direction 
-        if(i-1>=0 and !vis[i-1][j] and a[i-1][j]==1)
+        // Leftward
+        if(j-1>=0 and vis[i][j-1]==0 and grid[i][j-1]==1)
         {
             vis[i][j]=1;
-            solve(i-1,j,a,n,ans,move+'U',vis);
+            
+            ds.push_back('L');
+            f(i,j-1,grid,n,ans,ds,vis);
+            ds.pop_back();
+            
             vis[i][j]=0;
         }
     }
-    
     vector<string> findPath(vector<vector<int>> &m, int n)
     {
-        vector<string>ans;
+        vector<string> ans;
+        string ds = "";
+        
         vector<vector<int>>vis(n,vector<int>(n,0));
         
         if(m[0][0]==1)
         {
-            solve(0,0,m,n,ans,"",vis);
+            f(0,0,m,n,ans,ds,vis);
         }
+    
         return ans;
     }
+    
 };
 
-    
+
 // optimised approach 
 // instead of using 4 if statements using a for loop and a string ir ="DLRU"
-// using di and dj array 
-class Solution{
-    public:
-    // tc : o(4 ^(m*n))
-    // sc: O(m*n) ,Maximum Depth of the recursion tree(auxiliary space).
-    void solve(int i,int j,vector<vector<int>> &a,int n,vector<string>&ans,string move,vector<vector<int>>&vis,int di[],int dj[])
+// using dx and dy array 
+class Solution
+{
+public:
+    
+    void f(int i ,int j,vector<vector<int>> &grid ,int n  ,vector<string> &ans,string ds ,vector<vector<int>> &vis,int dx[],int dy[])
     {
+        // base case 
         if(i==n-1 and j==n-1)
         {
-            ans.push_back(move);
+            ans.push_back(ds);
             return;
         }
         
-        string dir ="DLRU";
+        string x = "ULDR";
         for(int ind=0;ind<4;ind++)
         {
-            int nexti= i+di[ind];
-            int nextj= j+dj[ind];
-            if(nexti>=0 and nextj>=0 and nexti<n and nextj<n and
-            !vis[nexti][nextj] and a[nexti][nextj]==1)
+            int nx = i + dx[ind];
+            int ny = j + dy[ind];
+            
+            if(nx>=0 and ny>=0 and nx<n and ny<n and grid[nx][ny]==1 and vis[nx][ny]==0)
             {
                 vis[i][j]=1;
-                solve(nexti,nextj,a,n,ans,move +dir[ind],vis,di,dj);
+                
+                f(nx,ny,grid,n,ans,ds + x[ind] ,vis,dx,dy);
+                
                 vis[i][j]=0;
             }
-        }        
+        }
     }
     
     vector<string> findPath(vector<vector<int>> &m, int n)
     {
-        vector<string>ans;
+        vector<string> ans;
+        string ds = "";
+        
         vector<vector<int>>vis(n,vector<int>(n,0));
-        int di[] ={1,0,0,-1};
-        int dj[] ={0,-1,1,0};
+        
+        int dx[4] = {-1,0,1,0};
+        int dy[4] = {0,-1,0,1};
+        
+        // int dx[4] = {1,0,0,-1};
+        // int dy[4] = {0,-1,1,0};
+        
+        
         if(m[0][0]==1)
         {
-            solve(0,0,m,n,ans,"",vis,di,dj);
+            f(0,0,m,n,ans,ds,vis,dx,dy);
         }
+    
         return ans;
     }
 };
