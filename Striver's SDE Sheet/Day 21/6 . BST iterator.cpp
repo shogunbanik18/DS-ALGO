8dc832e8cfc84647cbc86d73730921a8939f0012
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-05 20:39:28 +0530

    Create 6 . BST iterator.cpp

---
+++ Striver's SDE Sheet/Day 21/6 . BST iterator.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Designing of BST iterator
+// tc : o(1)
+// sc : o(H)
+class BSTIterator 
+{
+private:
+    stack<TreeNode*>st;
+    
+    void pushll(TreeNode* root)
+    {
+        while(root!=NULL)
+        {
+            st.push(root);
+            root= root->left;
+        }
+    }
+    
+public:
+    
+    BSTIterator(TreeNode* root)
+    {
+        pushll(root);
+    }
+   
+    int next()
+    {
+        int ans = 0 ;
+    
+        auto a = st.top();
+        ans = a->val;
+        st.pop();
+            
+        pushll(a->right);
+        
+        return ans;
+    }
+    
+    bool hasNext() 
+    {
+        if(!st.empty())
+        {
+            return true;
+        }
+        
+        else
+        {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
