b05b11eb3e4a5cfe51f35420ba83d1205832271c
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-09 16:36:43 +0530

    Create 4 .Distinct numbers in Window.cpp

---
+++ Striver's SDE Sheet/Day 22/4 .Distinct numbers in Window.cpp
@@ -0,0 +1,100 @@
+GFG
+
+// Brute Force 
+// tc : o(n^2)
+// sc : o(1)
+class Solution{
+  public:
+    vector <int> countDistinct (int A[], int n, int k)
+    {
+        vector<int> v;
+        
+        for(int i=0;i<= n-k;i++)
+        {
+            set<int>set;
+            for(int j = i;j<=(i+k-1) ;j++)
+            {
+                set.insert(A[j]);
+            }
+            v.push_back(set.size());
+        }
+        return v;
+    }
+};
+
+
+// using hashing 
+// tc : o(nlogn)
+// sc : o(1)
+// class Solution{
+//   public:
+//     vector <int> countDistinct (int A[], int n, int k)
+//     {
+//       vector<int>v;
+//       map<int,int>mp;
+       
+//       for(int i=0;i<k;i++)
+//       {
+//           mp[ A[i] ]++;
+//       }
+       
+//       v.push_back(mp.size());
+       
+//       for(int i=k;i<n;i++)
+//       {
+//           if(mp[A[i-k]] ==1 )
+//           {
+//               mp.erase(A[i-k]);
+//           }
+//           else
+//           {
+//               mp[A[i-k]]--;
+//           }
+           
+//             mp[A[i]]++;
+           
+//             v.push_back(mp.size());
+//       }
+       
+//     //   cout<<mp.size()<<endl;
+//       return v;
+//     }
+// };
+
+// using hashing 
+class Solution{
+  public:
+    vector <int> countDistinct (int A[], int n, int k)
+    {
+       vector<int>v;
+       map<int,int>mp;
+       
+       for(int i=0;i<k;i++)
+       {
+           mp[ A[i] ]++;
+       }
+       
+       v.push_back(mp.size());
+       
+       for(int i=k;i<n;i++)
+       {
+           if(mp.find(A[i-k]) != mp.end())
+           {
+               if(mp[A[i-k]] ==1 )
+               {
+                   mp.erase(A[i-k]);
+               }
+               else
+               {
+                   mp[A[i-k]]--;
+               }
+           }
+            mp[A[i]]++;
+           
+            v.push_back(mp.size());
+       }
+       
+    //   cout<<mp.size()<<endl;
+       return v;
+    }
+};
