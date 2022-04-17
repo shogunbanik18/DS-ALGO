1d925299e288a6527ed1807b43c7d0a4999482c5
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-17 17:54:44 +0530

    Update 11. Bipartite Check using BFS.cpp

--- Striver's SDE Sheet/Day 23/11. Bipartite Check using BFS.cpp
+++ Striver's SDE Sheet/Day 23/11. Bipartite Check using BFS.cpp
@@ -49,3 +49,74 @@ public:
 	    return true;
 	}
 };
+
+
+// leetcode
+// using bfs 
+// tc : o(n+e)
+// sc : o(n+e) + o(n) + o(n)
+class Solution 
+{
+public:
+    // using bfs taversal 
+    bool check_bipartite(int node ,vector<vector<int>>& graph , vector<int>&col)
+    {
+        col[node] =1;
+        queue<int> q;
+        
+        q.push(node);
+        
+        while(!q.empty())
+        {
+            auto a = q.front();
+            q.pop();
+            
+            for(auto x : graph[a])
+            {
+                if(col[x]==-1)
+                {
+                    col[x]= 1-col[a];
+                    q.push(x);
+                }
+                
+                else if(col[x] == col[a])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int n = graph.size();
+        int m = graph[0].size();
+        
+//         vector<int> adj[n];
+//         for(int i = 0;i<n;i++)
+//         {
+//             for(int j = 0;j<m;j++)
+//             {
+//                 adj[i].push_back(j);
+//                 adj[j].push_back(i);
+//             }
+//         }
+        
+        vector<int> col(n,-1);
+        for(int i =0;i<n;i++)
+        {
+            if(col[i]==-1)
+            {
+                col[i] = 1;
+                if(!check_bipartite(i,graph,col))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
