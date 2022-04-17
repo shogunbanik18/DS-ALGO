a7e7b1eba7d27151c7cb5d8a21e07b0bded48a0a
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-17 18:32:02 +0530

    Update 12 .Bipartite Check using DFS.cpp

--- Striver's SDE Sheet/Day 23/12 .Bipartite Check using DFS.cpp
+++ Striver's SDE Sheet/Day 23/12 .Bipartite Check using DFS.cpp
@@ -49,3 +49,71 @@ public:
 	    return true;
 	}
 };
+
+
+// Leetcode 
+// using Dfs
+// tc : o(n+e)
+// sc : o(n+e) + o(n) + o(n)
+class Solution 
+{
+public:
+    // using dfs taversal 
+    bool dfs(int node ,vector<vector<int>>& graph , vector<int>&col)
+    {
+        if(col[node]==-1)
+        {
+               col[node] =1;
+         }
+           
+        for(auto x : graph[node])
+        {
+            if(col[x]==-1)
+            {
+                col[x] = 1 - col[node];
+                if(!dfs(x,graph,col))
+                {
+                    return false;
+                }
+            }
+              
+            else if(col[x] == col[node])
+            {
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        int n = graph.size();
+        int m = graph[0].size();
+        
+//         vector<int> adj[n];
+//         for(int i = 0;i<n;i++)
+//         {
+//             for(int j = 0;j<m;j++)
+//             {
+//                 adj[i].push_back(j);
+//                 adj[j].push_back(i);
+//             }
+//         }
+        
+        vector<int> col(n,-1);
+        for(int i =0;i<n;i++)
+        {
+            if(col[i]==-1)
+            {
+                col[i] = 1;
+                if(!dfs(i,graph,col))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
