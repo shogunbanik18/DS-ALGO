c473dd97a3bc1ea9a18120f0cf9ab8f1b2f9adde
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-18 16:59:58 +0530

    Update 5 .Course Schedule_dfs.cpp

--- Striver's SDE Sheet/Day 23/5 .Course Schedule_dfs.cpp
+++ Striver's SDE Sheet/Day 23/5 .Course Schedule_dfs.cpp
@@ -1,3 +1,72 @@
+// Error Arises due to undirected graph traversal 
+
+// using directed graph 
+// tc : o(n+e)
+// sc : o(n+e) + o(n) + o(n)
+class Solution 
+{
+public:
+    
+    bool dfs_cycle(int node, vector<int>adj[] ,vector<int>&vis,vector<int>&dfs_vis)
+    {
+        vis[node] = 1;
+        dfs_vis[node] = 1;
+        for(auto x : adj[node])
+        {
+            if(!vis[x])
+            {
+                vis[x]=1;
+                dfs_vis[x] = 1;
+                if(dfs_cycle(x,adj,vis,dfs_vis))
+                {
+                    return true;
+                }
+            }
+            
+            else if(dfs_vis[x])
+            {
+                return true;
+            }
+        }
+        
+        // backtracking step 
+        dfs_vis[node] = 0;
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& pre)
+    {
+        int n = numCourses;
+        
+        vector<int> vis(n,0);
+        vector<int> dfs_vis(n,0);
+        
+        vector<int> adj[n];
+        
+        for(auto x : pre)
+        {
+            adj[x[1]].push_back(x[0]);
+        }
+        
+        for(int i = 0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                if( dfs_cycle(i,adj,vis,dfs_vis))
+                {
+                    return false;
+                }
+            }
+            
+        }
+        return true;
+    }
+};
+
+
+
+
+
 // using dfs traversal
 // tc : o(n+e)
 // sc : o(n+e) + o(n) + o(n)
