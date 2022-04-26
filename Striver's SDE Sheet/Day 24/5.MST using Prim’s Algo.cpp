9fc61c2a8984ec0b04607a8277d670031fa11462
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-04-26 16:53:37 +0530

    Update 5.MST using Prim’s Algo.cpp

--- Striver's SDE Sheet/Day 24/5.MST using Prim’s Algo.cpp
+++ Striver's SDE Sheet/Day 24/5.MST using Prim’s Algo.cpp
@@ -131,3 +131,137 @@ int main()
         }
     }
 }
+
+
+// gfg 
+// // // Graph to tree Based Conversion 
+// class Solution
+// {
+// 	public:
+// 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    
+//     int prism_algo(int src,int V, vector<vector<int>> adj[])
+//     {
+//         vector<int>parent(V);
+//         vector<int>key(V);
+//         vector<bool>mst(V);
+        
+//         for(int i=0;i<V;i++)
+//         {
+//             key[i] = INT_MAX;
+//             parent[i] = -1;
+//             mst[i] = false;
+//         }
+        
+//         key[src] = 0;
+        
+//         // edges are V-1
+//         for(int count = 0;count<V-1;count++)
+//         {
+//             int mini = INT_MAX,u;
+            
+//             for(int j=0;j<V;j++)
+//             {
+//                 if(mst[j]==false and key[j]<mini)
+//                 {
+//                     key[j]=mini;
+//                     u = j;
+//                 }
+//             }
+            
+//             mst[u] = true;
+//             for(auto x : adj[u])
+//             {
+//                 int a = x[0];
+//                 int weight = x[1];
+                
+//                 if(mst[a]==false and weight<key[a])
+//                 {
+//                     parent[a] = u;
+//                     key[a] = weight;
+//                 }
+//             }
+//         }
+        
+//         for(auto x : key)
+//         {
+//             cout<<x<<" ";
+//         }
+//         cout<<endl;
+        
+//         int sum = 0;
+//         for(int i = 0;i<V;i++)
+//         {
+//             sum +=key[i]; 
+//         }
+        
+//         return sum;
+//     }
+    
+//     int spanningTree(int V, vector<vector<int>> adj[])
+//     {
+//         return prism_algo(0,V,adj);
+//     }
+// };
+
+
+// Graph to tree Based Conversion
+// optimisation using priority queue
+// tc : o(n+e) + o(nlogn)
+// sc : o(n*n)
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int prism_algo(int src, int V, vector<vector<int>> adj[])
+    {
+        vector<int>parent(V);
+        vector<int>key(V);
+        vector<bool>mst(V);
+        
+        for(int i=0;i<V;i++)
+        {
+            key[i] = INT_MAX;
+            parent[i] = -1;
+            mst[i] = false;
+        }
+        
+        key[src] = 0;
+        
+        priority_queue< pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > pq;
+        pq.push({key[src],src});
+        
+        while(!pq.empty())
+        {
+            auto u = pq.top().second;
+            pq.pop();
+            
+            mst[u] = true;
+            for(auto x : adj[u])
+            {
+                auto a = x[0];
+                auto weight = x[1];
+                
+                if(mst[a]==false and weight<key[a])
+                {
+                    key[a] = weight;
+                    pq.push({key[a],a});
+                }
+            }
+        }
+        
+        int sum = 0;
+        for(int i = 0;i<V;i++)
+        {
+            sum +=key[i]; 
+        }
+        
+        return sum;
+    }
+    
+    
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        return prism_algo(0,V,adj);
+    }
+};
