689548a4536fb322962b2dba6a41b3dcd96cd5d9
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-05-06 21:01:39 +0530

    Update fibonacci series_entire sequence.cpp

--- Luv Babbar dsa/Dynamic Programming/fibonacci series_entire sequence.cpp
+++ Luv Babbar dsa/Dynamic Programming/fibonacci series_entire sequence.cpp
@@ -1,129 +1,95 @@
+// using recursion 
 #include<bits/stdc++.h>
 using namespace std;
 
-// using  Iteration
-// Bottom up dp 
-void fib(int n)
-{
-    int prev2=0,prev1=1;
-    for(int i=0;i<n;i++)
-    {
-        cout<<prev2<<" ";
-        int curr=prev2+prev1;
-        prev2=prev1;
-        prev1=curr;
-    }
-}
+// int fib(int n)
+// {
+//     if(n==0)
+//     {
+//         return 0;
+//     }
+    
+//     if(n==1)
+//     {
+//         return 1;
+//     }
+    
+//     return fib(n-1) + fib(n-2);
+// }
 
-int main()
-{
-    int n;
-    cin>>n;
-    fib(n);
-    return 0;
-}
+// // memoisation 
+// int fib(int n,vector<int>&dp)
+// {
+//     if(n==0)
+//     {
+//         return 0;
+//     }
+    
+//     if(n==1)
+//     {
+//         return 1;
+//     }
+    
+//     if(dp[n]!=-1)
+//     {
+//         return dp[n];
+//     }
+    
+//     return fib(n-1,dp) + fib(n-2,dp);
+// }
 
-// using recursion 
+// // tabulation dp 
+// int fib(int n,vector<int>&dp)
+// {
+//     dp[0] = 0;
+//     dp[1] = 1;
+    
+//     for(int i = 2; i<=n; i++)
+//     {
+//         dp[i] = dp[i-1] + dp[i-2];
+//     }
+    
+//     return dp[n];
+// }
+
+// without recursion 
+// tabulation dp + space optimisation 
+// tc : o(n)
+// sc : o(1)
 int fib(int n)
 {
     if(n==0)
     {
         return 0;
     }
+    
     if(n==1)
     {
         return 1;
     }
-    return fib(n-1)+fib(n-2);
+    
+    int prev2 = 0;
+    int prev1 = 1;
+    
+    for(int i = 2; i<=n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
 }
 
 int main()
-{   
+{
     int n;
     cin>>n;
-    for(int i=0;i<n;i++)
+    
+    for(int i = 0;i<=n;i++)
     {
         cout<<fib(i)<<" ";
+        // cout<<a<<" ";
     }
-    return 0;
-}
-
-// Using dynamic programming 
-// memoisation Top Down 
-int fib(int n,vector<int>&dp)
-{
-    if(n==0)
-    {
-        return 0;
-    }
-    if(n==1)
-    {
-        return 1;
-    }
-    if(dp[n]!=-1)
-    {
-        return dp[n];
-    }
-    return dp[n]=fib(n-1,dp)+fib(n-2,dp);
-}
-
-int main()
-{   
-    int n;
-    cin>>n;
-    vector<int>dp(n,-1);
-    for(int i=0;i<n;i++)
-    {
-        cout<<fib(i,dp)<<" ";
-    }
-    return 0;
-}
-
-// Using dynamic programming 
-// Tabulation Bottom up  
-int fib(int n,vector<int>&dp)
-{
-    dp[0]=0;
-    dp[1]=1;
-    for(int i=2;i<=n;i++)
-    {
-        dp[i]=dp[i-1]+dp[i-2];
-    }
-    return dp[n-1];
-}
-
-int main()
-{   
-    int n;
-    cin>>n;
-    vector<int>dp(n+1,-1);
-    
-    for(int i=1;i<n;i++)
-    {
-        cout<<fib(i,dp)<<" ";
-    }
-    return 0;
-}
-
-// Using dynamic programming 
-// Tabulation Bottom up 
-// Space optimisation 
-void fib(int n)
-{
-    int prev2=0,prev1=1;
-    for(int i=0;i<n;i++)
-    {
-        cout<<prev2<<" ";
-        int curr=prev2+prev1;
-        prev2=prev1;
-        prev1=curr;
-    }
-}
-
-int main()
-{   
-    int n;
-    cin>>n;
-    fib(n);
-    return 0;
+   
+    return 0 ;
 }
