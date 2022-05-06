7291b161dc76d4bef92dc84f9a1c9f9122766055
Shogun Banik <75260179+shogunbanik18@users.noreply.github.com>
2022-05-06 22:15:25 +0530

    Update HCF_LCM.cpp

--- Striver's SDE Sheet/HCF_LCM.cpp
+++ Striver's SDE Sheet/HCF_LCM.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// using iteration 
-// tc : o(n)
-// sc : o(1)
-int gcd1(int a,int b)
-{
-    int ans = 0;
-    for(int i = 1;i<=min(a,b);i++)
-    {
-        if(a%i==0 and b%i==0)
-        {
-            ans = i;
-        }
-    }
-    return ans;
-}
+// // using iteration 
+// // tc : o(n)
+// // sc : o(1)
+// int gcd1(int a,int b)
+// {
+//     int ans = 0;
+//     for(int i = 1;i<=min(a,b);i++)
+//     {
+//         if(a%i==0 and b%i==0)
+//         {
+//             ans = i;
+//         }
+//     }
+//     return ans;
+// }
 
 // using recursion 
 // tc : o(log(min(a,b)))
@@ -35,11 +35,25 @@ int gcd1(int a,int b)
 //     return gcd(a,b%a);
 // }
 
+
+// using recursion 
+// tc : o(log(min(a,b)))
+// sc : o(1)
+int gcd(int a ,int b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+    
+    return gcd(b,a%b);
+}
+
 // lcm * hcf = a*b 
 
 int lcm(int a,int b)
 {
-    int p = (a*b)/gcd1(a,b);
+    int p = (a*b)/gcd(a,b);
     return p;
 }
 
@@ -48,7 +62,7 @@ int main()
     int a,b;
     cin>>a>>b;
     
-    int hcf = gcd1(a,b);
+    int hcf = gcd(a,b);
     int lcm1 = lcm(a,b);
     cout<<hcf<<" "<<lcm1<<endl;
     return 0;
